723714a1d1668d1fc8feeea45ea83e7638a17e5c
IThundxr <me@ithundxr.dev>
2024-11-14 21:14:50 -0500

    Update javadocs

--- fabric-api-dev/src/main/java/net/fabricmc/fabric/api/dev/v1/FabricDevProperties.java
+++ fabric-api-dev/src/main/java/net/fabricmc/fabric/api/dev/v1/FabricDevProperties.java
@@ -33,7 +33,6 @@ public class FabricDevProperties {
 	/**
 	 * Logs an error when a weight is set to zero</br>
 	 * Property: <code>fabric.dev.zeroWeightWarning</code></br>
-	 * Default value: true</br>
 	 * {@link Weight#validate(int)}
 	 */
 	public static final boolean ZERO_WEIGHT_WARNING = getProperty("zeroWeightWarning");
@@ -41,7 +40,6 @@ public class FabricDevProperties {
 	/**
 	 * Logs an error when a translation is missing</br>
 	 * Property: <code>fabric.dev.logMissingTranslations</code></br>
-	 * Default value: true</br>
 	 * {@link Bootstrap#logMissing()}
 	 */
 	public static final boolean LOG_MISSING_TRANSLATIONS = getProperty("logMissingTranslations");
@@ -49,7 +47,6 @@ public class FabricDevProperties {
 	/**
 	 * Logs an error if Block classes don't end with "Block" and if Item classes don't end with "Item"</br>
 	 * Property: <code>fabric.dev.logConventionIssues</code></br>
-	 * Default value: true</br>
 	 * {@link net.minecraft.block.Block#Block} and {@link net.minecraft.item.Item#Item}
 	 */
 	public static final boolean LOG_BLOCK_AND_ITEM_CONVENTION_ISSUES = getProperty("logBlockAndItemConventionIssues");
@@ -60,7 +57,6 @@ public class FabricDevProperties {
 	 * WardenSpawnTrackerCommand, SpawnArmorTrimsCommand, ServerPackCommand),
 	 * and if on the server DebugConfigCommand</br>
 	 * Property: <code>fabric.dev.registerDebugCommands</code></br>
-	 * Default value: true</br>
 	 * {@link CommandManager#CommandManager}
 	 */
 	public static final boolean REGISTER_DEBUG_COMMANDS = getProperty("registerDebugCommands");
@@ -68,7 +64,6 @@ public class FabricDevProperties {
 	/**
 	 * Logs an error if a command threw an exception</br>
 	 * Property: <code>fabric.dev.enableCommandExceptionLogging</code></br>
-	 * Default value: true</br>
 	 * {@link CommandManager#execute(ParseResults, String)}
 	 */
 	public static final boolean ENABLE_COMMAND_EXCEPTION_LOGGING = getProperty("enableCommandExceptionLogging");
@@ -76,7 +71,6 @@ public class FabricDevProperties {
 	/**
 	 * Logs an error regarding argument ambiguity and throws an exception if an argument type is not registered</br>
 	 * Property: <code>fabric.dev.enableCommandArgumentLogging</code></br>
-	 * Default value: true</br>
 	 * {@link CommandManager#checkMissing()}
 	 */
 	public static final boolean ENABLE_COMMAND_ARGUMENT_LOGGING = getProperty("enableCommandArgumentLogging");
@@ -84,7 +78,6 @@ public class FabricDevProperties {
 	/**
 	 * Throw's an exception if a bounding box is invalid</br>
 	 * Property: <code>fabric.dev.throwOnInvalidBlockBoxes</code></br>
-	 * Default value: true</br>
 	 * {@link net.minecraft.util.math.BlockBox#BlockBox(int, int, int, int, int, int)}
 	 */
 	public static final boolean THROW_ON_INVALID_BLOCK_BOXES = getProperty("throwOnInvalidBlockBoxes");
@@ -92,7 +85,6 @@ public class FabricDevProperties {
 	/**
 	 * Logs an error if the heightmap is null</br>
 	 * Property: <code>fabric.dev.enableUnprimedHeightmapLogging</code></br>
-	 * Default value: true</br>
 	 * {@link net.minecraft.world.chunk.Chunk#sampleHeightmap(Heightmap.Type, int, int)}
 	 */
 	public static final boolean ENABLE_UNPRIMED_HEIGHTMAP_LOGGING = getProperty("enableUnprimedHeightmapLogging");
@@ -100,7 +92,6 @@ public class FabricDevProperties {
 	/**
 	 * Set's the current thread's name to the activeThreadName if debugRunnable or debugSupplier is called</br>
 	 * Property: <code>fabric.dev.enableSupplierAndRunnableDebugging</code></br>
-	 * Default value: false</br>
 	 * {@link net.minecraft.util.Util#debugRunnable(String, Runnable)} and {@link net.minecraft.util.Util#debugSupplier(String, Supplier)}
 	 */
 	public static final boolean ENABLE_SUPPLIER_AND_RUNNABLE_DEBUGGING = getProperty("enableSupplierAndRunnableDebugging");
@@ -109,7 +100,6 @@ public class FabricDevProperties {
 	 * Invokes a method in which you should have a breakpoint to debug errors
 	 * thrown with Util#error and exceptions thrown with Util#throwOrPause</br>
 	 * Property: <code>fabric.dev.enableExceptionIdePausing</code></br>
-	 * Default value: true</br>
 	 * {@link net.minecraft.util.Util#error(String)}, {@link net.minecraft.util.Util#error(String, Throwable)}
 	 * and {@link net.minecraft.util.Util#throwOrPause(Throwable)}
 	 */
