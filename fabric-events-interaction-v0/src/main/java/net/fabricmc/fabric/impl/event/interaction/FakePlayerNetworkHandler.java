Sinytra/FAPI-Mojmaps

Don't track fake player network addons (#3977)
* Don't track fake player network addons

* Review feedback

fabric-events-interaction-v0/src/main/java/net/fabricmc/fabric/impl/event/interaction/FakePlayerNetworkHandler.java
@@ -16,6 +16,8 @@
 
 package net.fabricmc.fabric.impl.event.interaction;
 
+import org.jetbrains.annotations.Nullable;
+import net.fabricmc.fabric.impl.networking.UntrackedNetworkHandler;
 import net.minecraft.network.Connection;
 import net.minecraft.network.PacketSendListener;
 import net.minecraft.network.protocol.Packet;
@@ -23,9 +25,8 @@ import net.minecraft.network.protocol.PacketFlow;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.server.network.CommonListenerCookie;
 import net.minecraft.server.network.ServerGamePacketListenerImpl;
-import org.jetbrains.annotations.Nullable;
 
-public class FakePlayerNetworkHandler extends ServerGamePacketListenerImpl {
+public final class FakePlayerNetworkHandler extends ServerGamePacketListenerImpl implements UntrackedNetworkHandler {
 	private static final Connection FAKE_CONNECTION = new FakeClientConnection();
 
 	public FakePlayerNetworkHandler(ServerPlayer player) {

FabricMC/fabric-api

fabric-events-interaction-v0/src/main/java/net/fabricmc/fabric/impl/event/interaction/FakePlayerNetworkHandler.java
@@ -25,7 +25,9 @@ import net.minecraft.network.packet.Packet;
 import net.minecraft.server.network.ServerPlayNetworkHandler;
 import net.minecraft.server.network.ServerPlayerEntity;
 
-public class FakePlayerNetworkHandler extends ServerPlayNetworkHandler {
+import net.fabricmc.fabric.impl.networking.UntrackedNetworkHandler;
+
+public final class FakePlayerNetworkHandler extends ServerPlayNetworkHandler implements UntrackedNetworkHandler {
 	private static final ClientConnection FAKE_CONNECTION = new ClientConnection(NetworkSide.CLIENTBOUND);
 
 	public FakePlayerNetworkHandler(ServerPlayerEntity player) {