6926072de8e7b4780476d405168be03ddfa386c0
Kevinthegreat <92656833+kevinthegreat1@users.noreply.github.com>
2025-01-11 15:16:58 -0500

    Fix checkstyle

--- fabric-rendering-v1/src/testmodClient/java/net/fabricmc/fabric/test/rendering/client/HudRenderEventsTests.java
+++ fabric-rendering-v1/src/testmodClient/java/net/fabricmc/fabric/test/rendering/client/HudRenderEventsTests.java
@@ -28,32 +28,31 @@ public class HudRenderEventsTests implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		HudLayerRegistrationCallback.EVENT.register(layeredDrawer -> layeredDrawer.addLayerBefore(IdentifiedLayer.MISC_OVERLAYS, Identifier.of("fabric", "test_before_misc_overlay"), (context, tickCounter) -> {
-					// Render a blue rectangle at the top right of the screen, and it should be blocked by misc overlays such as vignette, spyglass, and powder snow
-					context.fill(context.getScaledWindowWidth() - 200, 0, context.getScaledWindowWidth(), 30, Colors.BLUE);
-					context.drawTextWithShadow(MinecraftClient.getInstance().textRenderer, "1. Blue rectangle blocked by overlays", context.getScaledWindowWidth() - 196, 10, Colors.WHITE);
-					context.drawTextWithShadow(MinecraftClient.getInstance().textRenderer, "such as powder snow", context.getScaledWindowWidth() - 111, 20, Colors.WHITE);
-				}).addLayerAfter(IdentifiedLayer.MISC_OVERLAYS, Identifier.of("fabric", "test_after_misc_overlay"), (context, tickCounter) -> {
-					// Render a red square in the center of the screen underneath the crosshair
-					context.fill(context.getScaledWindowWidth() / 2 - 10, context.getScaledWindowHeight() / 2 - 10, context.getScaledWindowWidth() / 2 + 10, context.getScaledWindowHeight() / 2 + 10, Colors.RED);
-					context.drawCenteredTextWithShadow(MinecraftClient.getInstance().textRenderer, "2. Red square underneath crosshair", context.getScaledWindowWidth() / 2, context.getScaledWindowHeight() / 2 + 10, Colors.WHITE);
-				}).addLayerAfter(IdentifiedLayer.EXPERIENCE_LEVEL, Identifier.of("fabric", "test_after_experience_level"), (context, tickCounter) -> {
-					// Render a green rectangle at the bottom of the screen, and it should block the hotbar and status bars
-					context.fill(context.getScaledWindowWidth() / 2 - 50, context.getScaledWindowHeight() - 50, context.getScaledWindowWidth() / 2 + 50, context.getScaledWindowHeight() - 10, Colors.GREEN);
-					context.drawCenteredTextWithShadow(MinecraftClient.getInstance().textRenderer, "3. This green rectangle should block the hotbar and status bars.", context.getScaledWindowWidth() / 2, context.getScaledWindowHeight() - 40, Colors.WHITE);
-				}).addLayerBefore(IdentifiedLayer.DEMO_TIMER, Identifier.of("fabric", "test_before_demo_timer"), (context, tickCounter) -> {
-					// Render a yellow rectangle at the right of the screen, and it should be above the sleep overlay but below the scoreboard
-					context.fill(context.getScaledWindowWidth() - 240, context.getScaledWindowHeight() / 2 - 10, context.getScaledWindowWidth(), context.getScaledWindowHeight() / 2 + 10, Colors.YELLOW);
-					context.drawTextWithShadow(MinecraftClient.getInstance().textRenderer, "4. This yellow rectangle should be above", context.getScaledWindowWidth() - 236, context.getScaledWindowHeight() / 2 - 10, Colors.WHITE);
-					context.drawTextWithShadow(MinecraftClient.getInstance().textRenderer, "the sleep overlay but below the scoreboard.", context.getScaledWindowWidth() - 236, context.getScaledWindowHeight() / 2, Colors.WHITE);
-				}).addLayerBefore(IdentifiedLayer.CHAT, Identifier.of("fabric", "test_before_chat"), (context, tickCounter) -> {
-					// Render a blue rectangle at the bottom left of the screen, and it should be blocked by the chat
-					context.fill(0, context.getScaledWindowHeight() - 40, 300, context.getScaledWindowHeight() - 50, Colors.BLUE);
-					context.drawTextWithShadow(MinecraftClient.getInstance().textRenderer, "5. This blue rectangle should be blocked by the chat.", 0, context.getScaledWindowHeight() - 50, Colors.WHITE);
-				}).addLayerAfter(IdentifiedLayer.SUBTITLES, Identifier.of("fabric", "test_after_subtitles"), (context, tickCounter) -> {
-					// Render a yellow rectangle at the top of the screen, and it should block the player list
-					context.fill(context.getScaledWindowWidth() / 2 - 150, 0, context.getScaledWindowWidth() / 2 + 150, 15, Colors.YELLOW);
-					context.drawCenteredTextWithShadow(MinecraftClient.getInstance().textRenderer, "6. This yellow rectangle should block the player list.", context.getScaledWindowWidth() / 2, 0, Colors.WHITE);
-				})
-		);
+			// Render a blue rectangle at the top right of the screen, and it should be blocked by misc overlays such as vignette, spyglass, and powder snow
+			context.fill(context.getScaledWindowWidth() - 200, 0, context.getScaledWindowWidth(), 30, Colors.BLUE);
+			context.drawTextWithShadow(MinecraftClient.getInstance().textRenderer, "1. Blue rectangle blocked by overlays", context.getScaledWindowWidth() - 196, 10, Colors.WHITE);
+			context.drawTextWithShadow(MinecraftClient.getInstance().textRenderer, "such as powder snow", context.getScaledWindowWidth() - 111, 20, Colors.WHITE);
+		}).addLayerAfter(IdentifiedLayer.MISC_OVERLAYS, Identifier.of("fabric", "test_after_misc_overlay"), (context, tickCounter) -> {
+			// Render a red square in the center of the screen underneath the crosshair
+			context.fill(context.getScaledWindowWidth() / 2 - 10, context.getScaledWindowHeight() / 2 - 10, context.getScaledWindowWidth() / 2 + 10, context.getScaledWindowHeight() / 2 + 10, Colors.RED);
+			context.drawCenteredTextWithShadow(MinecraftClient.getInstance().textRenderer, "2. Red square underneath crosshair", context.getScaledWindowWidth() / 2, context.getScaledWindowHeight() / 2 + 10, Colors.WHITE);
+		}).addLayerAfter(IdentifiedLayer.EXPERIENCE_LEVEL, Identifier.of("fabric", "test_after_experience_level"), (context, tickCounter) -> {
+			// Render a green rectangle at the bottom of the screen, and it should block the hotbar and status bars
+			context.fill(context.getScaledWindowWidth() / 2 - 50, context.getScaledWindowHeight() - 50, context.getScaledWindowWidth() / 2 + 50, context.getScaledWindowHeight() - 10, Colors.GREEN);
+			context.drawCenteredTextWithShadow(MinecraftClient.getInstance().textRenderer, "3. This green rectangle should block the hotbar and status bars.", context.getScaledWindowWidth() / 2, context.getScaledWindowHeight() - 40, Colors.WHITE);
+		}).addLayerBefore(IdentifiedLayer.DEMO_TIMER, Identifier.of("fabric", "test_before_demo_timer"), (context, tickCounter) -> {
+			// Render a yellow rectangle at the right of the screen, and it should be above the sleep overlay but below the scoreboard
+			context.fill(context.getScaledWindowWidth() - 240, context.getScaledWindowHeight() / 2 - 10, context.getScaledWindowWidth(), context.getScaledWindowHeight() / 2 + 10, Colors.YELLOW);
+			context.drawTextWithShadow(MinecraftClient.getInstance().textRenderer, "4. This yellow rectangle should be above", context.getScaledWindowWidth() - 236, context.getScaledWindowHeight() / 2 - 10, Colors.WHITE);
+			context.drawTextWithShadow(MinecraftClient.getInstance().textRenderer, "the sleep overlay but below the scoreboard.", context.getScaledWindowWidth() - 236, context.getScaledWindowHeight() / 2, Colors.WHITE);
+		}).addLayerBefore(IdentifiedLayer.CHAT, Identifier.of("fabric", "test_before_chat"), (context, tickCounter) -> {
+			// Render a blue rectangle at the bottom left of the screen, and it should be blocked by the chat
+			context.fill(0, context.getScaledWindowHeight() - 40, 300, context.getScaledWindowHeight() - 50, Colors.BLUE);
+			context.drawTextWithShadow(MinecraftClient.getInstance().textRenderer, "5. This blue rectangle should be blocked by the chat.", 0, context.getScaledWindowHeight() - 50, Colors.WHITE);
+		}).addLayerAfter(IdentifiedLayer.SUBTITLES, Identifier.of("fabric", "test_after_subtitles"), (context, tickCounter) -> {
+			// Render a yellow rectangle at the top of the screen, and it should block the player list
+			context.fill(context.getScaledWindowWidth() / 2 - 150, 0, context.getScaledWindowWidth() / 2 + 150, 15, Colors.YELLOW);
+			context.drawCenteredTextWithShadow(MinecraftClient.getInstance().textRenderer, "6. This yellow rectangle should block the player list.", context.getScaledWindowWidth() / 2, 0, Colors.WHITE);
+		}));
 	}
 }
