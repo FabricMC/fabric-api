8420d9ef0af27bcdaa2843eeb07e2a3e86ea1778
shadowfflare <henk.wever18@gmail.com>
2025-01-29 19:28:48 +0100

    update FabricLoader

--- moltenex-wrapper/src/main/java/net/fabricmc/loader/api/FabricLoader.java
+++ moltenex-wrapper/src/main/java/net/fabricmc/loader/api/FabricLoader.java
@@ -4,6 +4,6 @@ import com.moltenex.loader.api.MoltenexLoader;
 
 public class FabricLoader {
 	public static MoltenexLoader getInstance(){
-		return com.moltenex.loader.api.MoltenexLoader.Companion.getInstance();
+		return MoltenexLoader.Companion.getInstance();
 	}
 }
