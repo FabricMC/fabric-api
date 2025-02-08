66e04f70dfa6bb8bfba557a2bbd09f2a8be5bf3c
Patbox <39821509+Patbox@users.noreply.github.com>
2025-02-08 21:04:51 +0100

    Fix checkstyle

--- fabric-item-api-v1/src/testmod/java/net/fabricmc/fabric/test/item/CustomModelIdTest.java
+++ fabric-item-api-v1/src/testmod/java/net/fabricmc/fabric/test/item/CustomModelIdTest.java
@@ -16,14 +16,14 @@
 
 package net.fabricmc.fabric.test.item;
 
-import net.fabricmc.api.ModInitializer;
-
-import net.minecraft.item.*;
+import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
+
+import net.fabricmc.api.ModInitializer;
 
 public class CustomModelIdTest implements ModInitializer {
 	public static final RegistryKey<Item> NOT_A_DIAMOND_KEY = RegistryKey.of(RegistryKeys.ITEM, Identifier.of("fabric-item-api-v1-testmod", "not_a_diamond"));
