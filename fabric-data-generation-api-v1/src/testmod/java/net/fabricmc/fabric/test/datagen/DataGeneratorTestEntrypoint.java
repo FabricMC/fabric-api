a63321bf5538951858de7c0ad560cbdb970c9c16
apple502j <33279053+apple502j@users.noreply.github.com>
2025-05-01 03:01:23 +0900

    [BREAKING] Re-implement forceAddTag and setReplace
    - Re-implement FabricProvidedTagBuilder as interface injection
    - Remove var-arg variant of forceAddTag since it causes SafeVarargs issue (and can be easily replaced)
    - Remove obsolete TAW

--- fabric-data-generation-api-v1/src/testmod/java/net/fabricmc/fabric/test/datagen/DataGeneratorTestEntrypoint.java
+++ fabric-data-generation-api-v1/src/testmod/java/net/fabricmc/fabric/test/datagen/DataGeneratorTestEntrypoint.java
@@ -307,10 +307,9 @@ public class DataGeneratorTestEntrypoint implements DataGeneratorEntrypoint {
 
 		@Override
 		protected void configure(RegistryWrapper.WrapperLookup registries) {
-			// TODO 1.21.6 fix me!!!
-			// tag(BlockTags.FIRE).setReplace(true).add(SIMPLE_BLOCK);
+			valueLookupBuilder(BlockTags.FIRE).setReplace(true).add(SIMPLE_BLOCK);
 			valueLookupBuilder(BlockTags.DIRT).add(SIMPLE_BLOCK);
-			// tag(BlockTags.ACACIA_LOGS).forceAddTag(BlockTags.ANIMALS_SPAWNABLE_ON);
+			valueLookupBuilder(BlockTags.ACACIA_LOGS).forceAddTag(BlockTags.ANIMALS_SPAWNABLE_ON);
 
 			aliasGroup("flowers")
 					.add(BlockTags.FLOWERS, BlockTags.FLOWER_POTS);
