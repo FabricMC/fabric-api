44c02c229fc34589388101bb1b49c5172ef6d93d
Kevinthegreat <92656833+kevinthegreat1@users.noreply.github.com>
2025-06-04 12:43:29 +0800

    Access widen ScissorStack and add Javadocs

--- fabric-rendering-v1/src/client/java/net/fabricmc/fabric/api/client/rendering/v1/SpecialGuiElementRendererRegistrationCallback.java
+++ fabric-rendering-v1/src/client/java/net/fabricmc/fabric/api/client/rendering/v1/SpecialGuiElementRendererRegistrationCallback.java
@@ -25,7 +25,18 @@ import net.minecraft.client.render.VertexConsumerProvider;
 import net.fabricmc.fabric.api.event.Event;
 import net.fabricmc.fabric.api.event.EventFactory;
 
+/**
+ * Allows registering and modifying {@link SpecialGuiElementRenderer special gui element renderers},
+ * used to render custom gui elements beyond the methods available in {@link net.minecraft.client.gui.DrawContext DrawContext}.
+ *
+ * <p>To render a custom gui element, first implement and register a {@link SpecialGuiElementRenderer}.
+ * When you want to render, add an instance of the corresponding render state to {@link net.minecraft.client.gui.DrawContext#state DrawContext#state} using {@link net.minecraft.client.gui.render.state.GuiRenderState#addSpecialElement(net.minecraft.client.gui.render.state.special.SpecialGuiElementRenderState) GuiRenderState#addSpecialElement(SpecialGuiElementRenderState)}.
+ */
 public class SpecialGuiElementRendererRegistrationCallback {
+	/**
+	 * Event for registering and modifying special gui element renderers.
+	 * Add a renderer by adding it to the provided list.
+	 */
 	public static final Event<BiConsumer<List<SpecialGuiElementRenderer<?>>, VertexConsumerProvider.Immediate>> EVENT = EventFactory.createArrayBacked(BiConsumer.class, listeners -> (specialElements, immediate) -> {
 		for (BiConsumer<List<SpecialGuiElementRenderer<?>>, VertexConsumerProvider.Immediate> listener : listeners) {
 			listener.accept(specialElements, immediate);
