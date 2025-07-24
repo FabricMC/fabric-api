3a78ff31028bccebe051a6cab32b150dd705bad8
Ram <104311709+RamGemes@users.noreply.github.com>
2025-07-24 13:31:28 -0400

    Fixed mixin location and changed injection to MEV

--- fabric-command-api-v2/src/main/java/net/fabricmc/fabric/mixin/command/CommandSourceMixin.java
+++ fabric-command-api-v2/src/main/java/net/fabricmc/fabric/mixin/command/CommandSourceMixin.java
@@ -14,10 +14,9 @@
  * limitations under the License.
  */
 
-package net.fabricmc.fabric.mixin.command.client;
+package net.fabricmc.fabric.mixin.command;
 
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
@@ -27,11 +26,11 @@ import net.minecraft.command.CommandSource;
 public interface CommandSourceMixin {
 	// Minecraft is hardcoded to only autofill identifiers with the "minecraft" namespace.
 	// This allows non-vanilla identifiers to be autofilled
-	@WrapOperation(
+	@ModifyExpressionValue(
 			method = "forEachMatching(Ljava/lang/Iterable;Ljava/lang/String;Ljava/util/function/Function;Ljava/util/function/Consumer;)V",
 			at = @At(value = "INVOKE", target = "Ljava/lang/String;equals(Ljava/lang/Object;)Z", ordinal = 0)
 	)
-	private static boolean cancelNamespaceCheck(String instance, Object o, Operation<Boolean> original) {
+	private static boolean cancelNamespaceCheck(boolean original) {
 		return true;
 	}
 }
