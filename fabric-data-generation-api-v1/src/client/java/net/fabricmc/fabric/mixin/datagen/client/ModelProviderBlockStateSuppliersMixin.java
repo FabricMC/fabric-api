3dd1547450d22a4dfddab0f97e92ac9f52cff9ea
Up <upcraftedlp@gmail.com>
2025-07-28 15:05:01 +0200

    replace Redirect with WrapOperation

--- fabric-data-generation-api-v1/src/client/java/net/fabricmc/fabric/mixin/datagen/client/ModelProviderBlockStateSuppliersMixin.java
+++ fabric-data-generation-api-v1/src/client/java/net/fabricmc/fabric/mixin/datagen/client/ModelProviderBlockStateSuppliersMixin.java
@@ -17,16 +17,16 @@
 package net.fabricmc.fabric.mixin.datagen.client;
 
 import java.util.function.Predicate;
-import java.util.stream.Stream;
+
+import net.minecraft.block.Block;
+import net.minecraft.registry.entry.RegistryEntry;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 
-import net.minecraft.block.Block;
 import net.minecraft.client.data.ModelProvider;
-import net.minecraft.registry.entry.RegistryEntry;
 
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.impl.datagen.client.FabricModelProviderDefinitions;
@@ -42,21 +42,14 @@ public class ModelProviderBlockStateSuppliersMixin implements FabricModelProvide
 	}
 
 	// Target the first .filter() call, to filter out blocks that are not from the mod we are processing.
-	@Redirect(method = "validate", at = @At(value = "INVOKE", target = "Ljava/util/stream/Stream;filter(Ljava/util/function/Predicate;)Ljava/util/stream/Stream;", ordinal = 0, remap = false))
-	private Stream<RegistryEntry.Reference<Block>> filterBlocksForProcessingMod(Stream<RegistryEntry.Reference<Block>> instance, Predicate<RegistryEntry.Reference<Block>> predicate) {
-		return instance.filter((block) -> {
-			if (fabricDataOutput != null) {
-				if (!fabricDataOutput.isStrictValidationEnabled()) {
-					return false;
-				}
-
-				if (!block.registryKey().getValue().getNamespace().equals(fabricDataOutput.getModId())) {
+	@ModifyArg(method = "validate", at = @At(value = "INVOKE", target = "Ljava/util/stream/Stream;filter(Ljava/util/function/Predicate;)Ljava/util/stream/Stream;", ordinal = 0, remap = false))
+	private Predicate<RegistryEntry.Reference<Block>> filterBlocksForProcessingMod(Predicate<RegistryEntry.Reference<Block>> original) {
+		if (fabricDataOutput != null) {
+			return block -> fabricDataOutput.isStrictValidationEnabled()
 					// Skip over blocks that are not from the mod we are processing.
-					return false;
-				}
-			}
+					&& block.registryKey().getValue().getNamespace().equals(fabricDataOutput.getModId());
+		}
 
-			return predicate.test(block);
-		});
+		return original;
 	}
 }
