97cdac944b3534b8c43ce88ed3bc64247f713e6f
Drex <nicknamedrex@gmail.com>
2025-11-04 21:29:23 +0100

    fabric-client-gametest-api-v1

--- fabric-transfer-api-v1/src/main/java/net/fabricmc/fabric/mixin/transfer/ListInventoryMixin.java
+++ fabric-transfer-api-v1/src/main/java/net/fabricmc/fabric/mixin/transfer/ListInventoryMixin.java
@@ -27,7 +27,7 @@ import net.fabricmc.fabric.impl.transfer.item.SpecialLogicAccess;
 
 @Mixin(ListBackedContainer.class)
 interface ListInventoryMixin extends ListBackedContainer, SpecialLogicAccess {
-	@WrapOperation(method = "setItem", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/entity/ListBackedContainer;markDirty()V"))
+	@WrapOperation(method = "setItem", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/entity/ListBackedContainer;setChanged()V"))
 	private void cancelMarkDirty(ListBackedContainer instance, Operation<Void> original) {
 		if (!this.fabric_shouldSuppressSpecialLogic()) {
 			original.call(instance);
