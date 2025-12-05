d84c0d82a71391bd952b58bd5910255b40d1df84
pistonpoek <pistonpoek@gmail.com>
2025-12-05 12:44:11 +0100

    Update SoundTypeBuilder.java
    Add API note to clarify the existence of the category field.

--- fabric-data-generation-api-v1/src/client/java/net/fabricmc/fabric/api/client/datagen/v1/builder/SoundTypeBuilder.java
+++ fabric-data-generation-api-v1/src/client/java/net/fabricmc/fabric/api/client/datagen/v1/builder/SoundTypeBuilder.java
@@ -67,6 +67,9 @@ public interface SoundTypeBuilder {
 	 * Sets the sound category the sound event must play on.
 	 *
 	 * <p>The default category is {@link SoundSource#NEUTRAL}. GUI elements should use {@link SoundSource#MASTER}.
+	 *
+	 * @apiNote Category is not used by vanilla for the sound event registration,
+	 * but can be specified for fabric and other mods to use.
 	 */
 	SoundTypeBuilder category(SoundSource category);
 
