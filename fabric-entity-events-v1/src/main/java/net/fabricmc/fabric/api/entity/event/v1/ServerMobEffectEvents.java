67f557c606777b23107faecd16cc3c4fd6c0debd
sylv256 <contact@sylv.gay>
2025-12-06 01:13:00 -0500

    docs: `ServerMobEffectEvents`

--- fabric-entity-events-v1/src/main/java/net/fabricmc/fabric/api/entity/event/v1/ServerMobEffectEvents.java
+++ fabric-entity-events-v1/src/main/java/net/fabricmc/fabric/api/entity/event/v1/ServerMobEffectEvents.java
@@ -26,9 +26,26 @@ import net.fabricmc.fabric.api.event.EventFactory;
 /**
  * Events related to {@linkplain MobEffect status effects} in mobs.
  *
- * <p>TODO: improve this, look at {@link EntitySleepEvents} for inspiration
+ * <p>Status effect events are useful when implementing generic behavior for
+ * many status effects and modifying the addition and removal behavior of
+ * existing status effects.
+ *
+ * <p>When only one class of {@linkplain MobEffect status effects}
+ * requires code to be run before the addition or removal of the effect,
+ * consider using {@link MobEffect#onEffectAdded(MobEffectInstance, LivingEntity)}
+ * or {@link MobEffect#onEffectRemoved(MobEffectInstance, LivingEntity)}.
+ *
+ * @see FabricMobEffectEventHandlers
  */
 public final class ServerMobEffectEvents {
+	/**
+	 * An event that checks whether an effect may be added.
+	 *
+	 * <p>This event can be used to no-op effects given specific
+	 * conditions such as an item held, a
+	 * {@linkplain net.fabricmc.fabric.api.attachment.v1.AttachmentType data attachment},
+	 * or another status effect.
+	 */
 	public static final Event<AllowAdd> ALLOW_ADD = EventFactory.createArrayBacked(AllowAdd.class, callbacks -> (effectInstance, entity) -> {
 		for (AllowAdd callback : callbacks) {
 			if (!callback.allowAdd(effectInstance, entity)) {
@@ -39,12 +56,28 @@ public final class ServerMobEffectEvents {
 		return true;
 	});
 
+	/**
+	 * An event that is called before an effect is added.
+	 *
+	 * <p>This event can be used to generalize behavior normally
+	 * in the {@link MobEffect#onEffectAdded} methods to a subset
+	 * of status effects or even to all status effects.
+	 *
+	 * @see MobEffect#onEffectAdded(MobEffectInstance, LivingEntity)
+	 */
 	public static final Event<BeforeAdd> BEFORE_ADD = EventFactory.createArrayBacked(BeforeAdd.class, callbacks -> (effectInstance, entity) -> {
 		for (BeforeAdd callback : callbacks) {
 			callback.beforeAdd(effectInstance, entity);
 		}
 	});
 
+	/**
+	 * An event that is called after an effect is added.
+	 *
+	 * <p>This event is useful for code that is required to run
+	 * only after an effect is applied such as a method that
+	 * checks if an effect is present on an entity.
+	 */
 	public static final Event<AfterAdd> AFTER_ADD = EventFactory.createArrayBacked(AfterAdd.class, callbacks -> (effectInstance, entity) -> {
 		for (AfterAdd callback : callbacks) {
 			callback.afterAdd(effectInstance, entity);
@@ -61,12 +94,28 @@ public final class ServerMobEffectEvents {
 		return true;
 	});
 
+	/**
+	 * An event that is called before an effect is removed.
+	 *
+	 * <p>This event can be used to generalize behavior normally
+	 * in the {@link MobEffect#onEffectRemoved} method to a subset
+	 * of status effects or even to all status effects.
+	 *
+	 * @see MobEffect#onEffectRemoved(MobEffectInstance, LivingEntity)
+	 */
 	public static final Event<BeforeRemove> BEFORE_REMOVE = EventFactory.createArrayBacked(BeforeRemove.class, callbacks -> (effectInstance, entity) -> {
 		for (BeforeRemove callback : callbacks) {
 			callback.beforeRemove(effectInstance, entity);
 		}
 	});
 
+	/**
+	 * An event that is called after an effect is removed.
+	 *
+	 * <p>This event is useful for code that is required to run
+	 * only after an effect is removed such as a method that
+	 * checks if an effect is present on an entity.
+	 */
 	public static final Event<AfterRemove> AFTER_REMOVE = EventFactory.createArrayBacked(AfterRemove.class, callbacks -> (effectInstance, entity) -> {
 		for (AfterRemove callback : callbacks) {
 			callback.afterRemove(effectInstance, entity);
@@ -87,31 +136,73 @@ public final class ServerMobEffectEvents {
 
 	@FunctionalInterface
 	public interface AllowAdd {
+		/**
+		 * Checks whether an effect may be added.
+		 *
+		 * @param effectInstance the instance of the status effect being added
+		 * @param entity the entity on which the effect is being added
+		 * @return whether this effect may be added
+		 */
 		boolean allowAdd(MobEffectInstance effectInstance, LivingEntity entity);
 	}
 
 	@FunctionalInterface
 	public interface BeforeAdd {
+		/**
+		 * Called before an effect is added.
+		 *
+		 * @param effectInstance the instance of the status effect being added
+		 * @param entity the entity on which the effect is being added
+		 */
 		void beforeAdd(MobEffectInstance effectInstance, LivingEntity entity);
 	}
 
 	@FunctionalInterface
 	public interface AfterAdd {
+		/**
+		 * Called after an effect is added.
+		 *
+		 * @param effectInstance the instance of the added status effect
+		 * @param entity the entity on which the effect has been added
+		 */
 		void afterAdd(MobEffectInstance effectInstance, LivingEntity entity);
 	}
 
 	@FunctionalInterface
 	public interface AllowEarlyRemove {
+		/**
+		 * Checks whether an effect may be removed early.
+		 *
+		 * <p><b>Note:</b> this event is not called when an
+		 * effect expires. The behavior of effect expiry
+		 * typically should not be modified.
+		 *
+		 * @param effectInstance the instance of the status effect being removed
+		 * @param entity the entity on which the effect is being removed
+		 * @return whether this effect may be removed
+		 */
 		boolean allowEarlyRemove(MobEffectInstance effectInstance, LivingEntity entity);
 	}
 
 	@FunctionalInterface
 	public interface BeforeRemove {
+		/**
+		 * Called before an effect is removed.
+		 *
+		 * @param effectInstance the instance of the status effect being removed
+		 * @param entity the entity on which the effect is being removed
+		 */
 		void beforeRemove(MobEffectInstance effectInstance, LivingEntity entity);
 	}
 
 	@FunctionalInterface
 	public interface AfterRemove {
+		/**
+		 * Called after an effect is removed.
+		 *
+		 * @param effectInstance the instance of the removed status effect
+		 * @param entity the entity from which the effect has been removed
+		 */
 		void afterRemove(MobEffectInstance effectInstance, LivingEntity entity);
 	}
 }
