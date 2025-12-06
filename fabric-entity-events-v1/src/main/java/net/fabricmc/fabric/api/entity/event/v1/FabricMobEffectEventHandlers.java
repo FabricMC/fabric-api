17bdec71bf35d881e26accbba612f8ffe1e3a7a2
sylv256 <contact@sylv.gay>
2025-12-05 21:57:29 -0500

    docs: `FabricMobEffectEventHandlers`

--- fabric-entity-events-v1/src/main/java/net/fabricmc/fabric/api/entity/event/v1/FabricMobEffectEventHandlers.java
+++ fabric-entity-events-v1/src/main/java/net/fabricmc/fabric/api/entity/event/v1/FabricMobEffectEventHandlers.java
@@ -8,9 +8,21 @@ import net.minecraft.world.entity.LivingEntity;
  * An extension for {@link MobEffect} subclasses adding basic events.
  */
 public interface FabricMobEffectEventHandlers {
+	/**
+	 * Called after an {@linkplain MobEffectInstance instance of this effect} has been added to a {@linkplain LivingEntity living entity}.
+	 *
+	 * @param effectInstance an instance of this effect
+	 * @param entity the entity the effect instance is being applied to
+	 */
 	default void onEffectAdded(MobEffectInstance effectInstance, LivingEntity entity) {
 	}
 
+	/**
+	 * Called after an {@linkplain MobEffectInstance instance of this effect} has been removed from a {@linkplain LivingEntity living entity}.
+	 *
+	 * @param effectInstance an instance of this effect
+	 * @param entity the entity the effect instance is being removed from
+	 */
 	default void onEffectRemoved(MobEffectInstance effectInstance, LivingEntity entity) {
 	}
 }
