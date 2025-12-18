67d4cfdc0f0368f08396e9b703439b6a2468a623
Cassian <cassian@godsted.com>
2025-12-17 20:20:37 -0500

    Update EquipmentSlotProvider.java

--- fabric-item-api-v1/src/main/java/net/fabricmc/fabric/api/item/v1/EquipmentSlotProvider.java
+++ fabric-item-api-v1/src/main/java/net/fabricmc/fabric/api/item/v1/EquipmentSlotProvider.java
@@ -26,7 +26,7 @@ import net.minecraft.world.item.ItemStack;
  * an armor slot that they can go in.
  *
  * <p>The preferred equipment slot of an item stack can be queried using
- * {@link LivingEntity#getEquipmentSlotForItem(ItemStack).
+ * {@link LivingEntity#getEquipmentSlotForItem(ItemStack)}.
  *
  * <p>Equipment slot providers can be set with {@link FabricItem.Properties#equipmentSlot(EquipmentSlotProvider)}.
  *
