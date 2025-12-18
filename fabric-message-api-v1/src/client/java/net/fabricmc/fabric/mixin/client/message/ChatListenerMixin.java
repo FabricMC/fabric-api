ddb228db9a25e70e16d07da9c2e9c8c34f19c10c
coredex-source <samdhi2323@gmail.com>
2025-12-18 23:08:09 +0530

    fabric-message-api-v1 - commit.4
    - Patched ChatListenerMixin.
    - Done with client renaming.

--- fabric-message-api-v1/src/client/java/net/fabricmc/fabric/mixin/client/message/ChatListenerMixin.java
+++ fabric-message-api-v1/src/client/java/net/fabricmc/fabric/mixin/client/message/ChatListenerMixin.java
@@ -39,30 +39,30 @@ import net.fabricmc.fabric.api.client.message.v1.ClientLogChatMessageEvents;
 @Mixin(ChatListener.class)
 public abstract class ChatListenerMixin {
 	@Inject(method = "showMessageToPlayer", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/Gui;getChat()Lnet/minecraft/client/gui/components/ChatComponent;", ordinal = 0), cancellable = true)
-	private void fabric_onSignedChatMessage(ChatType.Bound params, PlayerChatMessage message, Component decorated, GameProfile sender, boolean onlyShowSecureChat, Instant receptionTimestamp, CallbackInfoReturnable<Boolean> cir) {
-		fabric_onChatMessage(decorated, message, sender, params, receptionTimestamp, cir);
+	private void fabric_onSignedChatMessage(ChatType.Bound bind, PlayerChatMessage message, Component decorated, GameProfile sender, boolean onlyShowSecureChat, Instant timeStamp, CallbackInfoReturnable<Boolean> cir) {
+		fabric_onChatMessage(decorated, message, sender, bind, timeStamp, cir);
 	}
 
 	@Inject(method = "showMessageToPlayer", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/Gui;getChat()Lnet/minecraft/client/gui/components/ChatComponent;", ordinal = 1), cancellable = true)
-	private void fabric_onFilteredSignedChatMessage(ChatType.Bound params, PlayerChatMessage message, Component decorated, GameProfile sender, boolean onlyShowSecureChat, Instant receptionTimestamp, CallbackInfoReturnable<Boolean> cir) {
+	private void fabric_onFilteredSignedChatMessage(ChatType.Bound bind, PlayerChatMessage message, Component decorated, GameProfile sender, boolean onlyShowSecureChat, Instant timeStamp, CallbackInfoReturnable<Boolean> cir) {
 		Component filtered = message.filterMask().applyWithFormatting(message.signedContent());
 
 		if (filtered != null) {
-			fabric_onChatMessage(params.decorate(filtered), message, sender, params, receptionTimestamp, cir);
+			fabric_onChatMessage(bind.decorate(filtered), message, sender, bind, timeStamp, cir);
 		}
 	}
 
 	@Inject(method = "lambda$handleDisguisedChatMessage$0", at = @At("HEAD"), cancellable = true)
-	private void fabric_onProfilelessChatMessage(ChatType.Bound params, Component content, Instant receptionTimestamp, CallbackInfoReturnable<Boolean> cir) {
-		fabric_onChatMessage(params.decorate(content), null, null, params, receptionTimestamp, cir);
+	private void fabric_onProfilelessChatMessage(ChatType.Bound bind, Component content, Instant timeStamp, CallbackInfoReturnable<Boolean> cir) {
+		fabric_onChatMessage(bind.decorate(content), null, null, bind, timeStamp, cir);
 	}
 
 	@Unique
-	private void fabric_onChatMessage(Component message, @Nullable PlayerChatMessage signedMessage, @Nullable GameProfile sender, ChatType.Bound params, Instant receptionTimestamp, CallbackInfoReturnable<Boolean> cir) {
-		if (ClientLogChatMessageEvents.IS_CHAT_ALLOWED.invoker().allowLogChatMessage(message, signedMessage, sender, params, receptionTimestamp)) {
-			ClientLogChatMessageEvents.CHAT.invoker().onLogChatMessage(message, signedMessage, sender, params, receptionTimestamp);
+	private void fabric_onChatMessage(Component message, @Nullable PlayerChatMessage playerChatMessage, @Nullable GameProfile sender, ChatType.Bound bind, Instant timeStamp, CallbackInfoReturnable<Boolean> cir) {
+		if (ClientLogChatMessageEvents.IS_CHAT_ALLOWED.invoker().allowLogChatMessage(message, playerChatMessage, sender, bind, timeStamp)) {
+			ClientLogChatMessageEvents.CHAT.invoker().onLogChatMessage(message, playerChatMessage, sender, bind, timeStamp);
 		} else {
-			ClientLogChatMessageEvents.CHAT_CANCELED.invoker().onLogChatMessageCanceled(message, signedMessage, sender, params, receptionTimestamp);
+			ClientLogChatMessageEvents.CHAT_CANCELED.invoker().onLogChatMessageCanceled(message, playerChatMessage, sender, bind, timeStamp);
 			cir.setReturnValue(false);
 		}
 	}
