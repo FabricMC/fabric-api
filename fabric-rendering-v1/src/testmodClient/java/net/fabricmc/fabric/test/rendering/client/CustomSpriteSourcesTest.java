ae2cb87935376dec26b773d58235cfc279966bda
sylv256 <contact@sylv.gay>
2025-12-19 19:29:52 -0500

    refactor(rendering-v1): `AtlasSource` -> `SpriteSource`

--- fabric-rendering-v1/src/testmodClient/java/net/fabricmc/fabric/test/rendering/client/CustomSpriteSourcesTest.java
+++ fabric-rendering-v1/src/testmodClient/java/net/fabricmc/fabric/test/rendering/client/CustomSpriteSourcesTest.java
@@ -44,20 +44,20 @@ import net.fabricmc.fabric.api.client.rendering.v1.SpriteSourceRegistry;
 public class CustomSpriteSourcesTest implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
-		SpriteSourceRegistry.register(Identifier.fromNamespaceAndPath("fabric-rendering-v1-testmod", "double"), DoubleAtlasSource.CODEC);
+		SpriteSourceRegistry.register(Identifier.fromNamespaceAndPath("fabric-rendering-v1-testmod", "double"), DoubleSpriteSource.CODEC);
 	}
 
-	private static class DoubleAtlasSource implements SpriteSource {
+	private static class DoubleSpriteSource implements SpriteSource {
 		private static final Logger LOGGER = LogUtils.getLogger();
-		public static final MapCodec<DoubleAtlasSource> CODEC = RecordCodecBuilder.mapCodec(instance -> instance.group(
+		public static final MapCodec<DoubleSpriteSource> CODEC = RecordCodecBuilder.mapCodec(instance -> instance.group(
 				Identifier.CODEC.fieldOf("resource").forGetter(source -> source.resource),
 				Identifier.CODEC.fieldOf("sprite").forGetter(source -> source.sprite)
-		).apply(instance, DoubleAtlasSource::new));
+		).apply(instance, DoubleSpriteSource::new));
 
 		private final Identifier resource;
 		private final Identifier sprite;
 
-		DoubleAtlasSource(Identifier resource, Identifier sprite) {
+		DoubleSpriteSource(Identifier resource, Identifier sprite) {
 			this.resource = resource;
 			this.sprite = sprite;
 		}
