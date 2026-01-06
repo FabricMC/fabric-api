c971aae1300fb04d29addabb89a87ee99373aca5
Kilip1000 <kilian.lennard.pro@gmail.com>
2026-01-06 11:38:07 +0100

    fix

--- fabric-transfer-api-v1/src/main/java/net/fabricmc/fabric/api/transfer/v1/storage/Storage.java
+++ fabric-transfer-api-v1/src/main/java/net/fabricmc/fabric/api/transfer/v1/storage/Storage.java
@@ -91,7 +91,7 @@ public interface Storage<T> extends Iterable<StorageView<T>> {
 	long insert(T resource, long maxAmount, TransactionContext transaction);
 
 	/**
-	 * @return  {@code false} if calling {@link #extract} will absolutely always return 0, or {@code true} otherwise or in doubt.
+	 * Return {@code false} if calling {@link #extract} will absolutely always return 0, or {@code true} otherwise or in doubt.
 	 *
 	 * <p>Note: This function is meant to be used by pipes or other devices that can transfer resources to know if
 	 * they should interact with this storage at all.
