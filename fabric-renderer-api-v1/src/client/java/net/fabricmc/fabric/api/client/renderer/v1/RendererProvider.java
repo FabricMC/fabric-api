1f7eb43f9508605ae70416328bd76f5b042ceea6
sylv256 <contact@sylv.gay>
2026-02-04 19:42:48 -0500

    docs: Warn user about calling `RendererProvider#getRenderer` early

--- fabric-renderer-api-v1/src/client/java/net/fabricmc/fabric/api/client/renderer/v1/RendererProvider.java
+++ fabric-renderer-api-v1/src/client/java/net/fabricmc/fabric/api/client/renderer/v1/RendererProvider.java
@@ -37,6 +37,8 @@ public interface RendererProvider {
 	/**
 	 * Get or instantiate an implementation of {@link Renderer}.
 	 *
+	 * @apiNote Do not call this method before the {@link RendererReadyEntrypoint} has been invoked.
+	 *
 	 * @return an instance of the currently registered {@link Renderer}.
 	 * @implSpec This method should instantiate an implementation of {@link Renderer} the first time
 	 * it is invoked and return that instance for any subsequent calls.
