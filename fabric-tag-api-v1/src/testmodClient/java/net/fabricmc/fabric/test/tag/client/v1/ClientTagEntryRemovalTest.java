bf3e6f4fd27a3ee116db705f0b4565cd90873ec0
Anastasia Chrysanthemum <cow@chrysanthemum.lgbt>
2026-02-09 17:31:33 +1100

    Update exception messages.

--- fabric-tag-api-v1/src/testmodClient/java/net/fabricmc/fabric/test/tag/client/v1/ClientTagEntryRemovalTest.java
+++ fabric-tag-api-v1/src/testmodClient/java/net/fabricmc/fabric/test/tag/client/v1/ClientTagEntryRemovalTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2016, 2017, 2018, 2019 FabricMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package net.fabricmc.fabric.test.tag.client.v1;
 
 import net.fabricmc.api.ClientModInitializer;
@@ -29,10 +45,10 @@ public class ClientTagEntryRemovalTest implements ClientModInitializer {
 				throw new AssertionError("Expected not to find mud in fabric-tag-api-v1-testmod:dirt_and_mud_with_client_exclusions, but it was found!");
 			}
 			if (!ClientTags.isInWithLocalFallback(REMOVAL_TEST_TAG, Blocks.ROOTED_DIRT)) {
-				throw new AssertionError("Expected to find rooted_dirt in fabric-tag-api-v1-testmod:dirt_and_mud_with_client_exclusions, but it was found!");
+				throw new AssertionError("Expected to find rooted_dirt in fabric-tag-api-v1-testmod:dirt_and_mud_with_client_exclusions, but it was not found!");
 			}
 			if (!ClientTags.isInWithLocalFallback(REMOVAL_TEST_TAG, Blocks.MUDDY_MANGROVE_ROOTS)) {
-				throw new AssertionError("Expected to find muddy_mangrove_roots in fabric-tag-api-v1-testmod:dirt_and_mud_with_client_exclusions, but it was found!");
+				throw new AssertionError("Expected to find muddy_mangrove_roots in fabric-tag-api-v1-testmod:dirt_and_mud_with_client_exclusions, but it was not found!");
 			}
 
 			// Success!
