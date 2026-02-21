473bee427aba4eb4e88440a95c2bd7776573d195
bleudev <aitiiigg1@gmail.com>
2026-02-21 23:40:57 +0300

    Fix style in testmod

--- fabric-lifecycle-events-v1/src/testmod/java/net/fabricmc/fabric/test/event/lifecycle/ServerEntityLifecycleTests.java
+++ fabric-lifecycle-events-v1/src/testmod/java/net/fabricmc/fabric/test/event/lifecycle/ServerEntityLifecycleTests.java
@@ -52,8 +52,9 @@ public final class ServerEntityLifecycleTests implements ModInitializer {
 
 		ServerEntityEvents.ALLOW_FRESH_LOAD.register((entity, level) -> {
 			if (PRINT_SERVER_ENTITY_MESSAGES) {
-                logger.info("[SERVER] ALLOW FRESH LOAD {} IN {}", entity, level);
+				logger.info("[SERVER] ALLOW FRESH LOAD {} IN {}", entity, level);
 			}
+
 			return true;
 		});
 
