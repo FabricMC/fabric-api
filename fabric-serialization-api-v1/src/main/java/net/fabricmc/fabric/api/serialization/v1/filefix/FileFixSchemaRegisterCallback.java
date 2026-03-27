f454f0d1841ed34095c493e817cd70f7fa64adf4
Eclipse <eclipse@eclipseisoffline.xyz>
2026-03-27 10:22:23 +0000

    Add documentation to FileFixSchemaRegisterCallback

--- fabric-serialization-api-v1/src/main/java/net/fabricmc/fabric/api/serialization/v1/filefix/FileFixSchemaRegisterCallback.java
+++ fabric-serialization-api-v1/src/main/java/net/fabricmc/fabric/api/serialization/v1/filefix/FileFixSchemaRegisterCallback.java
@@ -10,6 +10,14 @@ import net.minecraft.util.filefix.FileFixerUpper;
 
 import java.util.function.Function;
 
+/**
+ * Event for registering file fixes. This event is called for every {@link Schema} added to
+ * Minecraft's {@link FileFixerUpper}, before any of vanilla's fixes are added to it.
+ *
+ * <p>Please note that this event is called early during the game launch, before {@link net.fabricmc.api.ModInitializer}s
+ * are run. Therefore, make sure to register callbacks early during the mod loading process, using
+ * a {@link net.fabricmc.loader.api.entrypoint.PreLaunchEntrypoint}.</p>
+ */
 @FunctionalInterface
 public interface FileFixSchemaRegisterCallback {
 
@@ -22,6 +30,16 @@ public interface FileFixSchemaRegisterCallback {
 
 	void schemaRegistered(FileFixerUpper.Builder fileFixerUpper, Schema schema, int version);
 
+	/**
+	 * Can be used to easily register file fixes for a data version. This method registers a callback
+	 * at {@link FileFixSchemaRegisterCallback#EVENT}, which adds the given {@code fixes} to the
+	 * game's {@link FileFixerUpper} at the given data version.
+	 *
+	 * <p>Please note that this method does not throw for invalid data versions.</p>
+	 *
+	 * @param version the data version to register the file fixes for.
+	 * @param fixes the file fixes to register.
+	 */
 	@SafeVarargs
 	static void registerFileFixes(int version, Function<Schema, FileFix>... fixes) {
 		EVENT.register((fileFixerUpper, schema, schemaVersion) -> {
