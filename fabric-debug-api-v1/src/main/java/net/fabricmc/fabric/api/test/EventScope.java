981c5149c548ea4f45e2527ee03b061af4efe9b4
Kilip1000 <206253641+Kilip1000@users.noreply.github.com>
2026-03-30 02:35:43 +0200

    chore: javadoc
     Initial work on javadoc for EventScopes.

--- fabric-debug-api-v1/src/main/java/net/fabricmc/fabric/api/test/EventScope.java
+++ fabric-debug-api-v1/src/main/java/net/fabricmc/fabric/api/test/EventScope.java
@@ -7,6 +7,11 @@ import net.minecraft.resources.Identifier;
 import net.fabricmc.fabric.api.event.Event;
 import net.fabricmc.fabric.impl.test.EventTestingImpl;
 
+/**
+ * Represents a wrapper around a short-lived {@link Event}.
+ * This class implements {@link AutoCloseable} and is intended to be used in a try-with-resources statement. When
+ * closed, the Event be unregistered.
+ */
 @ApiStatus.NonExtendable
 public interface EventScope extends AutoCloseable {
 	@Override
