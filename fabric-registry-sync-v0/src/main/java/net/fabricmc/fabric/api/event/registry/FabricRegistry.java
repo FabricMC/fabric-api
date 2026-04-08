Sinytra/FAPI-Mojmaps

[1.21.1] Registry aliasing backport (#4698)
* Registry aliasing (#4231)

* initial

* style

* tests + more input checking

* revert formatting

* requests + more tests

* testmod

* check not frozen

* Fixes

* throw on default impl

* optimize implementation

---------

Co-authored-by: modmuss50 <modmuss50@gmail.com>
(cherry picked from commit e2e49f78f18095fc9b4607a2061479f2d07053f2)

* Fix testmod

---------

Co-authored-by: Syst3ms <arthmm14@gmail.com>
Co-authored-by: modmuss50 <modmuss50@gmail.com>

fabric-registry-sync-v0/src/main/java/net/fabricmc/fabric/api/event/registry/FabricRegistry.java
@@ -1,0 +1,40 @@
+/*
+ * Copyright (c) 2016, 2017, 2018, 2019 FabricMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.fabricmc.fabric.api.event.registry;
+
+import net.minecraft.core.Registry;
+import net.minecraft.resources.ResourceLocation;
+import org.jetbrains.annotations.ApiStatus;
+
+/**
+ * General-purpose Fabric-provided extensions for {@link Registry} objects.
+ *
+ * <p>Note: This interface is automatically implemented on all registries via Mixin and interface injection.</p>
+ */
+@ApiStatus.NonExtendable
+public interface FabricRegistry {
+	/**
+	 * Adds an alias for an entry in this registry. Once added, all queries to this registry that refer to the {@code old}
+	 * {@link ResourceLocation} will be redirected towards {@code newId}. This is useful if a mod wants to change an ID without
+	 * breaking compatibility with existing worlds.
+	 * @param old the {@link ResourceLocation} that will become an alias for {@code newId}
+	 * @param newId the {@link ResourceLocation} for which {@code old} will become an alias
+	 */
+	default void addAlias(ResourceLocation old, ResourceLocation newId) {
+		throw new UnsupportedOperationException("implemented via mixin");
+	}
+}

fabric-registry-sync-v0/src/main/java/net/fabricmc/fabric/mixin/registry/sync/RegistryMixin.java
@@ -1,0 +1,25 @@
+/*
+ * Copyright (c) 2016, 2017, 2018, 2019 FabricMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.fabricmc.fabric.mixin.registry.sync;
+
+import org.spongepowered.asm.mixin.Mixin;
+import net.fabricmc.fabric.api.event.registry.FabricRegistry;
+import net.minecraft.core.Registry;
+
+@Mixin(Registry.class)
+public interface RegistryMixin extends FabricRegistry {
+}

fabric-registry-sync-v0/src/test/java/net/fabricmc/fabric/test/registry/sync/RegistryAliasTest.java
@@ -1,0 +1,101 @@
+/*
+ * Copyright (c) 2016, 2017, 2018, 2019 FabricMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.fabricmc.fabric.test.registry.sync;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.UUID;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import net.minecraft.SharedConstants;
+import net.minecraft.core.Registry;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.Bootstrap;
+import net.fabricmc.fabric.api.event.registry.FabricRegistryBuilder;
+
+public class RegistryAliasTest {
+	private static final ResourceLocation OBSOLETE_ID = id("obsolete");
+	private static final ResourceLocation NEW_ID = id("new");
+	private static final ResourceLocation OTHER = id("other");
+	private ResourceKey<Registry<String>> testRegistryKey;
+	private Registry<String> testRegistry;
+
+	@BeforeAll
+	static void beforeAll() {
+		SharedConstants.tryDetectVersion();
+		Bootstrap.bootStrap();
+	}
+
+	private static ResourceLocation id(String s) {
+		return ResourceLocation.fromNamespaceAndPath("registry_sync_test_alias_test", s);
+	}
+
+	@BeforeEach
+	void beforeEach() {
+		testRegistryKey = ResourceKey.createRegistryKey(id(UUID.randomUUID().toString()));
+		testRegistry = Mockito.spy(FabricRegistryBuilder.createSimple(testRegistryKey).buildAndRegister());
+
+		Registry.register(testRegistry, NEW_ID, "entry");
+		Registry.register(testRegistry, OTHER, "other");
+		testRegistry.addAlias(OBSOLETE_ID, NEW_ID);
+	}
+
+	@Test
+	void testAlias() {
+		ResourceKey<String> obsoleteKey = ResourceKey.create(testRegistryKey, OBSOLETE_ID);
+
+		assertTrue(testRegistry.containsKey(OBSOLETE_ID));
+		assertFalse(testRegistry.keySet().contains(OBSOLETE_ID));
+		assertEquals("entry", testRegistry.get(OBSOLETE_ID));
+		assertEquals("entry", testRegistry.get(obsoleteKey));
+
+		ResourceLocation moreObsolete = id("more_obsolete");
+		assertFalse(testRegistry.containsKey(moreObsolete));
+
+		testRegistry.addAlias(moreObsolete, OBSOLETE_ID);
+
+		assertTrue(testRegistry.containsKey(moreObsolete));
+		assertEquals("entry", testRegistry.get(moreObsolete));
+	}
+
+	@Test
+	void forbidAmbiguousAlias() {
+		assertThrows(IllegalArgumentException.class, () -> testRegistry.addAlias(OBSOLETE_ID, OTHER));
+	}
+
+	@Test
+	void forbidCircularAliases() {
+		assertThrows(IllegalArgumentException.class, () -> testRegistry.addAlias(NEW_ID, OBSOLETE_ID));
+	}
+
+	@Test
+	void forbidExistingIdAsAlias() {
+		assertThrows(IllegalArgumentException.class, () -> testRegistry.addAlias(NEW_ID, OTHER));
+	}
+
+	@Test
+	void forbidOverridingAliasWithEntry() {
+		assertThrows(IllegalArgumentException.class, () -> Registry.register(testRegistry, OBSOLETE_ID, "obsolete"));
+	}
+}

fabric-registry-sync-v0/src/testmod/java/net/fabricmc/fabric/test/registry/sync/RegistryAliasTest.java
@@ -1,0 +1,67 @@
+/*
+ * Copyright (c) 2016, 2017, 2018, 2019 FabricMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.fabricmc.fabric.test.registry.sync;
+
+import com.mojang.logging.LogUtils;
+import org.slf4j.Logger;
+import net.fabricmc.api.ModInitializer;
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+
+public class RegistryAliasTest implements ModInitializer {
+	private static final Logger LOGGER = LogUtils.getLogger();
+	public static final boolean USE_OLD_IDS = Boolean.parseBoolean(System.getProperty("fabric.registry.sync.test.alias.use_old_ids", "true"));
+	public static final ResourceLocation OLD_TEST_INGOT = id("test_ingot_old");
+	public static final ResourceLocation TEST_INGOT = id("test_ingot");
+	public static final ResourceLocation OLD_TEST_BLOCK = id("test_block_old");
+	public static final ResourceLocation TEST_BLOCK = id("test_block");
+
+	@Override
+	public void onInitialize() {
+		if (USE_OLD_IDS) {
+			LOGGER.info("Registering old IDs");
+			register(OLD_TEST_BLOCK, OLD_TEST_INGOT);
+		} else {
+			LOGGER.info("Registering new IDs");
+			register(TEST_BLOCK, TEST_INGOT);
+			LOGGER.info("Adding aliases");
+			BuiltInRegistries.BLOCK.addAlias(OLD_TEST_BLOCK, TEST_BLOCK);
+			BuiltInRegistries.ITEM.addAlias(OLD_TEST_BLOCK, TEST_BLOCK);
+			BuiltInRegistries.ITEM.addAlias(OLD_TEST_INGOT, TEST_INGOT);
+		}
+
+		BuiltInRegistries.ITEM.addAlias(ResourceLocation.parse("old_stone"), ResourceLocation.parse("stone"));
+	}
+
+	private static void register(ResourceLocation blockId, ResourceLocation itemId) {
+		Block block = new Block(BlockBehaviour.Properties.of());
+		Registry.register(BuiltInRegistries.BLOCK, blockId, block);
+		BlockItem blockItem = new BlockItem(block, new Item.Properties());
+		Registry.register(BuiltInRegistries.ITEM, blockId, blockItem);
+		Item item = new Item(new Item.Properties());
+		Registry.register(BuiltInRegistries.ITEM, itemId, item);
+	}
+
+	private static ResourceLocation id(String path) {
+		return ResourceLocation.fromNamespaceAndPath("registry_sync_alias_test", path);
+	}
+}

FabricMC/fabric-api

fabric-registry-sync-v0/src/main/java/net/fabricmc/fabric/api/event/registry/FabricRegistry.java
@@ -1,0 +1,41 @@
+/*
+ * Copyright (c) 2016, 2017, 2018, 2019 FabricMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.fabricmc.fabric.api.event.registry;
+
+import org.jetbrains.annotations.ApiStatus;
+
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+/**
+ * General-purpose Fabric-provided extensions for {@link Registry} objects.
+ *
+ * <p>Note: This interface is automatically implemented on all registries via Mixin and interface injection.</p>
+ */
+@ApiStatus.NonExtendable
+public interface FabricRegistry {
+	/**
+	 * Adds an alias for an entry in this registry. Once added, all queries to this registry that refer to the {@code old}
+	 * {@link Identifier} will be redirected towards {@code newId}. This is useful if a mod wants to change an ID without
+	 * breaking compatibility with existing worlds.
+	 * @param old the {@link Identifier} that will become an alias for {@code newId}
+	 * @param newId the {@link Identifier} for which {@code old} will become an alias
+	 */
+	default void addAlias(Identifier old, Identifier newId) {
+		throw new UnsupportedOperationException("implemented via mixin");
+	}
+}

fabric-registry-sync-v0/src/main/java/net/fabricmc/fabric/mixin/registry/sync/RegistryMixin.java
@@ -1,0 +1,27 @@
+/*
+ * Copyright (c) 2016, 2017, 2018, 2019 FabricMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.fabricmc.fabric.mixin.registry.sync;
+
+import org.spongepowered.asm.mixin.Mixin;
+
+import net.minecraft.registry.Registry;
+
+import net.fabricmc.fabric.api.event.registry.FabricRegistry;
+
+@Mixin(Registry.class)
+public interface RegistryMixin extends FabricRegistry {
+}

fabric-registry-sync-v0/src/test/java/net/fabricmc/fabric/test/registry/sync/RegistryAliasTest.java
@@ -1,0 +1,103 @@
+/*
+ * Copyright (c) 2016, 2017, 2018, 2019 FabricMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.fabricmc.fabric.test.registry.sync;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.UUID;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import net.minecraft.Bootstrap;
+import net.minecraft.SharedConstants;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.util.Identifier;
+
+import net.fabricmc.fabric.api.event.registry.FabricRegistryBuilder;
+
+public class RegistryAliasTest {
+	private static final Identifier OBSOLETE_ID = id("obsolete");
+	private static final Identifier NEW_ID = id("new");
+	private static final Identifier OTHER = id("other");
+	private RegistryKey<Registry<String>> testRegistryKey;
+	private Registry<String> testRegistry;
+
+	@BeforeAll
+	static void beforeAll() {
+		SharedConstants.createGameVersion();
+		Bootstrap.initialize();
+	}
+
+	private static Identifier id(String s) {
+		return Identifier.of("registry_sync_test_alias_test", s);
+	}
+
+	@BeforeEach
+	void beforeEach() {
+		testRegistryKey = RegistryKey.ofRegistry(id(UUID.randomUUID().toString()));
+		testRegistry = Mockito.spy(FabricRegistryBuilder.createSimple(testRegistryKey).buildAndRegister());
+
+		Registry.register(testRegistry, NEW_ID, "entry");
+		Registry.register(testRegistry, OTHER, "other");
+		testRegistry.addAlias(OBSOLETE_ID, NEW_ID);
+	}
+
+	@Test
+	void testAlias() {
+		RegistryKey<String> obsoleteKey = RegistryKey.of(testRegistryKey, OBSOLETE_ID);
+
+		assertTrue(testRegistry.containsId(OBSOLETE_ID));
+		assertFalse(testRegistry.getIds().contains(OBSOLETE_ID));
+		assertEquals("entry", testRegistry.get(OBSOLETE_ID));
+		assertEquals("entry", testRegistry.get(obsoleteKey));
+
+		Identifier moreObsolete = id("more_obsolete");
+		assertFalse(testRegistry.containsId(moreObsolete));
+
+		testRegistry.addAlias(moreObsolete, OBSOLETE_ID);
+
+		assertTrue(testRegistry.containsId(moreObsolete));
+		assertEquals("entry", testRegistry.get(moreObsolete));
+	}
+
+	@Test
+	void forbidAmbiguousAlias() {
+		assertThrows(IllegalArgumentException.class, () -> testRegistry.addAlias(OBSOLETE_ID, OTHER));
+	}
+
+	@Test
+	void forbidCircularAliases() {
+		assertThrows(IllegalArgumentException.class, () -> testRegistry.addAlias(NEW_ID, OBSOLETE_ID));
+	}
+
+	@Test
+	void forbidExistingIdAsAlias() {
+		assertThrows(IllegalArgumentException.class, () -> testRegistry.addAlias(NEW_ID, OTHER));
+	}
+
+	@Test
+	void forbidOverridingAliasWithEntry() {
+		assertThrows(IllegalArgumentException.class, () -> Registry.register(testRegistry, OBSOLETE_ID, "obsolete"));
+	}
+}

fabric-registry-sync-v0/src/testmod/java/net/fabricmc/fabric/test/registry/sync/RegistryAliasTest.java
@@ -1,0 +1,69 @@
+/*
+ * Copyright (c) 2016, 2017, 2018, 2019 FabricMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.fabricmc.fabric.test.registry.sync;
+
+import com.mojang.logging.LogUtils;
+import org.slf4j.Logger;
+
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+import net.fabricmc.api.ModInitializer;
+
+public class RegistryAliasTest implements ModInitializer {
+	private static final Logger LOGGER = LogUtils.getLogger();
+	public static final boolean USE_OLD_IDS = Boolean.parseBoolean(System.getProperty("fabric.registry.sync.test.alias.use_old_ids", "true"));
+	public static final Identifier OLD_TEST_INGOT = id("test_ingot_old");
+	public static final Identifier TEST_INGOT = id("test_ingot");
+	public static final Identifier OLD_TEST_BLOCK = id("test_block_old");
+	public static final Identifier TEST_BLOCK = id("test_block");
+
+	@Override
+	public void onInitialize() {
+		if (USE_OLD_IDS) {
+			LOGGER.info("Registering old IDs");
+			register(OLD_TEST_BLOCK, OLD_TEST_INGOT);
+		} else {
+			LOGGER.info("Registering new IDs");
+			register(TEST_BLOCK, TEST_INGOT);
+			LOGGER.info("Adding aliases");
+			Registries.BLOCK.addAlias(OLD_TEST_BLOCK, TEST_BLOCK);
+			Registries.ITEM.addAlias(OLD_TEST_BLOCK, TEST_BLOCK);
+			Registries.ITEM.addAlias(OLD_TEST_INGOT, TEST_INGOT);
+		}
+
+		Registries.ITEM.addAlias(Identifier.of("old_stone"), Identifier.of("stone"));
+	}
+
+	private static void register(Identifier blockId, Identifier itemId) {
+		Block block = new Block(AbstractBlock.Settings.create());
+		Registry.register(Registries.BLOCK, blockId, block);
+		BlockItem blockItem = new BlockItem(block, new Item.Settings());
+		Registry.register(Registries.ITEM, blockId, blockItem);
+		Item item = new Item(new Item.Settings());
+		Registry.register(Registries.ITEM, itemId, item);
+	}
+
+	private static Identifier id(String path) {
+		return Identifier.of("registry_sync_alias_test", path);
+	}
+}