7e75b696e19efab90c4213a49886ed589e85203e
Anastasia Chrysanthemum <cow@chrysanthemum.lgbt>
2026-04-09 23:19:33 +1000

    refactor(test): Use Furnace overlay sprites for BookCraftingOverlayRecipeComponent.

--- fabric-recipe-api-v1/src/testmodClient/java/net/fabricmc/fabric/test/recipe/client/book/BookCraftingOverlayRecipeComponent.java
+++ fabric-recipe-api-v1/src/testmodClient/java/net/fabricmc/fabric/test/recipe/client/book/BookCraftingOverlayRecipeComponent.java
@@ -24,19 +24,14 @@ import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.GuiGraphicsExtractor;
 import net.minecraft.client.gui.screens.recipebook.OverlayRecipeComponent;
 import net.minecraft.client.gui.screens.recipebook.SlotSelectTime;
-import net.minecraft.client.renderer.RenderPipelines;
 import net.minecraft.resources.Identifier;
 import net.minecraft.util.context.ContextMap;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.display.RecipeDisplay;
 import net.minecraft.world.item.crafting.display.RecipeDisplayEntry;
 import net.minecraft.world.item.crafting.display.RecipeDisplayId;
-import net.minecraft.world.item.crafting.display.SlotDisplay;
 
 import net.fabricmc.fabric.test.recipe.book.BookRecipeDisplay;
-import net.fabricmc.fabric.test.recipe.book.RecipeBookTestContent;
-import net.fabricmc.fabric.test.recipe.client.mixin.OverlayRecipeComponentAccessor;
-import net.fabricmc.fabric.test.recipe.client.mixin.OverlayRecipeComponentOverlayRecipeButtonAccessor;
 
 public class BookCraftingOverlayRecipeComponent extends OverlayRecipeComponent {
 	public BookCraftingOverlayRecipeComponent(SlotSelectTime slotSelectTime) {
@@ -49,10 +44,10 @@ public class BookCraftingOverlayRecipeComponent extends OverlayRecipeComponent {
 	}
 
 	public class BookCraftingOverlayRecipeButton extends OverlayRecipeButton {
-		private static final Identifier ENABLED_SPRITE = RecipeBookTestContent.id("recipe_book/book_crafting_overlay");
-		private static final Identifier HIGHLIGHTED_ENABLED_SPRITE = RecipeBookTestContent.id("recipe_book/book_crafting_overlay_highlighted");
-		private static final Identifier DISABLED_SPRITE = RecipeBookTestContent.id("recipe_book/book_crafting_overlay_disabled");
-		private static final Identifier HIGHLIGHTED_DISABLED_SPRITE = RecipeBookTestContent.id("recipe_book/book_crafting_overlay_disabled_highlighted");
+		private static final Identifier ENABLED_SPRITE = Identifier.withDefaultNamespace("recipe_book/furnace_overlay");
+		private static final Identifier HIGHLIGHTED_ENABLED_SPRITE = Identifier.withDefaultNamespace("recipe_book/furnace_overlay_highlighted");
+		private static final Identifier DISABLED_SPRITE = Identifier.withDefaultNamespace("recipe_book/furnace_overlay_disabled");
+		private static final Identifier HIGHLIGHTED_DISABLED_SPRITE = Identifier.withDefaultNamespace("recipe_book/furnace_overlay_disabled_highlighted");
 
 		private final Font font;
 		private final ItemStack result;
@@ -66,10 +61,7 @@ public class BookCraftingOverlayRecipeComponent extends OverlayRecipeComponent {
 		private static List<OverlayRecipeButton.Pos> calculateIngredientsPositions(final RecipeDisplay recipe, final ContextMap context) {
 			if (recipe instanceof BookRecipeDisplay bookRecipeDisplay) {
 				return List.of(
-						new OverlayRecipeButton.Pos(0, 0, bookRecipeDisplay.ingredient()
-								.resolve(context, SlotDisplay.ItemStackContentsFactory.INSTANCE)
-								.toList()
-						)
+						createGridPos(1, 1, bookRecipeDisplay.ingredient().resolveForStacks(context))
 				);
 			}
 			return Collections.emptyList();
@@ -77,21 +69,7 @@ public class BookCraftingOverlayRecipeComponent extends OverlayRecipeComponent {
 
 		@Override
 		public void extractWidgetRenderState(final GuiGraphicsExtractor graphics, final int mouseX, final int mouseY, final float a) {
-			graphics.blitSprite(RenderPipelines.GUI_TEXTURED, this.getSprite(((OverlayRecipeComponentOverlayRecipeButtonAccessor) this).getIsCraftable()), this.getX(), this.getY(), this.width, this.height);
-			float gridPosX = (float) (this.getX() + 2);
-			float gridPosY = (float) (this.getY() + 2);
-
-			if (((OverlayRecipeComponentOverlayRecipeButtonAccessor) this).getSlots().isEmpty())
-				return;
-
-			Pos pos = ((OverlayRecipeComponentOverlayRecipeButtonAccessor) this).getSlots().getFirst();
-
-			graphics.pose().pushMatrix();
-			graphics.pose().translate(gridPosX + (float) pos.x(), gridPosY + (float) pos.y());
-			graphics.pose().translate(2.0F, 2.0F);
-			graphics.item(pos.selectIngredient(((OverlayRecipeComponentAccessor) BookCraftingOverlayRecipeComponent.this).getSlotSelectTime().currentIndex()), 0, 0);
-			graphics.pose().popMatrix();
-
+			super.extractWidgetRenderState(graphics, mouseX, mouseY, a);
 			if (isHoveredOrFocused()) {
 				graphics.setTooltipForNextFrame(font, result, mouseX, mouseY);
 			}
