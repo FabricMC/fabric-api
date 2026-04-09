cc456af379b561f7d669fa1f9b9b9571d68f6307
Anastasia Chrysanthemum <cow@chrysanthemum.lgbt>
2026-04-09 23:39:02 +1000

    docs: Document FabricRecipeBookComponent.

--- fabric-recipe-api-v1/src/client/java/net/fabricmc/fabric/api/client/recipe/v1/book/FabricRecipeBookComponent.java
+++ fabric-recipe-api-v1/src/client/java/net/fabricmc/fabric/api/client/recipe/v1/book/FabricRecipeBookComponent.java
@@ -19,12 +19,23 @@ package net.fabricmc.fabric.api.client.recipe.v1.book;
 import net.minecraft.client.gui.screens.recipebook.OverlayRecipeComponent;
 import net.minecraft.client.gui.screens.recipebook.SlotSelectTime;
 
+/**
+ * Fabric-provided extensions for {@link net.minecraft.client.gui.screens.recipebook.RecipeBookComponent}.
+ */
 public interface FabricRecipeBookComponent {
 	default SlotSelectTime getSlotSelectTime() {
 		throw new AssertionError("Implemented via mixin");
 	}
 
-	default void setOverlay(OverlayRecipeComponent component) {
+	/**
+	 * Sets this book component's overlay.
+	 * <p>This is used for modifying the overlay upon right-clicking a recipe item button.
+	 *
+	 * @apiNote This should most commonly be called within the init method of your recipe book overlay.
+	 *
+	 * @param overlay An overlay recipe component.
+	 */
+	default void setOverlay(OverlayRecipeComponent overlay) {
 		throw new AssertionError("Implemented via mixin");
 	}
 }
