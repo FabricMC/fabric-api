4d18aba4a91bb5f5a8159794b4e5b8e28ec8e0ee
ThePotatoArchivist <josiahfu@gmail.com>
2026-05-18 20:07:37 -0700

    Fix duplicate remove entries

--- fabric-tag-api-v1/src/main/java/net/fabricmc/fabric/mixin/tag/TagLoaderMixin.java
+++ fabric-tag-api-v1/src/main/java/net/fabricmc/fabric/mixin/tag/TagLoaderMixin.java
@@ -65,7 +65,7 @@ public class TagLoaderMixin {
 	private <T> Either<List<TagLoader.EntryWithSource>, List<T>> scopeIdToTryBuildTag(TagLoader<T> instance, TagEntry.Lookup<T> lookup, List<TagLoader.EntryWithSource> entries, Operation<Either<List<TagLoader.EntryWithSource>, List<T>>> original, @Local(argsOnly = true) Identifier id) {
 		// List is merged here to make
 		return ScopedValue.where(TagRemovalInternals.TAG_ID_SCOPED_VALUE, id)
-				.call(() -> original.call(instance, lookup, TagRemovalInternals.mergeAddedAndRemovedEntries(id, entries)));
+				.call(() -> original.call(instance, lookup, entries));
 	}
 
 	@ModifyArg(method = "lambda$build$0", at = @At(value = "INVOKE", target = "Lnet/minecraft/tags/TagLoader$SortingEntry;<init>(Ljava/util/List;)V"))
