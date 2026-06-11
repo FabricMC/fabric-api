2b365509dbfd4a5fae542750192aa4cfe8d4e76f
EightSidedSquare <contact@eightsidedsquare.com>
2026-06-11 00:48:21 -0400

    Application and Ordering Game Tests

--- fabric-holder-component-api-v1/src/testmod/java/net/fabricmc/fabric/test/holder/component/HolderComponentGameTest.java
+++ fabric-holder-component-api-v1/src/testmod/java/net/fabricmc/fabric/test/holder/component/HolderComponentGameTest.java
@@ -16,20 +16,68 @@
 
 package net.fabricmc.fabric.test.holder.component;
 
+import net.minecraft.core.Holder;
 import net.minecraft.core.component.DataComponents;
 import net.minecraft.gametest.framework.GameTestHelper;
-import net.minecraft.network.chat.Component;
+import net.minecraft.world.entity.animal.pig.PigVariant;
+import net.minecraft.world.entity.animal.pig.PigVariants;
+import net.minecraft.world.item.DyeColor;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.block.Blocks;
 
 import net.fabricmc.fabric.api.gametest.v1.GameTest;
 
 public class HolderComponentGameTest {
 	@GameTest
-	public void testBasicUsage(GameTestHelper helper) {
+	public void testComponentChangesFromJSON(GameTestHelper helper) {
+		// We test for the addition a pig variant to ensure that dynamic registries are present
+		// when holder components are loaded from JSON
+		Holder<PigVariant> pigVariant = Items.WHEAT_SEEDS.get(DataComponents.PIG_VARIANT);
+		helper.assertTrue(
+				pigVariant != null && pigVariant.is(PigVariants.TEMPERATE),
+				"Wheat Seeds must have 'pig/variant' component with value 'temperate'"
+		);
+		helper.assertFalse(
+				Items.MELON_SEEDS.has(DataComponents.CUSTOM_NAME),
+				"Wheat Seeds must have its 'custom_name' component removed"
+		);
+
+		helper.succeed();
+	}
+
+	@GameTest
+	public void testComponentChangesFromInitializer(GameTestHelper helper) {
+		// We test for the addition a pig variant to ensure that DataComponentInitializers can both
+		// modify components and access dynamic registries
+		Holder<PigVariant> pigVariant = Items.MELON_SEEDS.get(DataComponents.PIG_VARIANT);
+		helper.assertTrue(
+				pigVariant != null && pigVariant.is(PigVariants.TEMPERATE),
+				"Melon Seeds must have 'pig/variant' component with value 'temperate'"
+		);
+		helper.assertFalse(
+				Items.MELON_SEEDS.has(DataComponents.CUSTOM_NAME),
+				"Melon Seeds must have its 'custom_name' component removed"
+		);
+
+		helper.succeed();
+	}
+
+	@GameTest
+	public void testInitializerOrdering(GameTestHelper helper) {
 		helper.assertValueEqual(
-				Blocks.ACACIA_SLAB.get(DataComponents.CUSTOM_NAME),
-				Component.literal("Hello, world!"),
-				"custom_name_component"
+				Blocks.DIRT.getOrDefault(DataComponents.CAT_COLLAR, DyeColor.WHITE),
+				DyeColor.RED,
+				"red_cat_collar"
+		);
+		helper.assertValueEqual(
+				Blocks.DIRT.getOrDefault(DataComponents.SHEEP_COLOR, DyeColor.WHITE),
+				DyeColor.GREEN,
+				"green_sheep_color"
+		);
+		helper.assertValueEqual(
+				Blocks.DIRT.getOrDefault(DataComponents.SHULKER_COLOR, DyeColor.WHITE),
+				DyeColor.BLUE,
+				"blue_shulker_color"
 		);
 
 		helper.succeed();
--- fabric-holder-component-api-v1/src/testmod/java/net/fabricmc/fabric/test/holder/component/HolderComponentTestModEntrypoint.java
+++ fabric-holder-component-api-v1/src/testmod/java/net/fabricmc/fabric/test/holder/component/HolderComponentTestModEntrypoint.java
@@ -16,11 +16,15 @@
 
 package net.fabricmc.fabric.test.holder.component;
 
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import net.minecraft.core.component.DataComponents;
+import net.minecraft.references.BlockIds;
+import net.minecraft.references.ItemIds;
 import net.minecraft.resources.Identifier;
+import net.minecraft.world.entity.animal.pig.PigVariants;
+import net.minecraft.world.item.DyeColor;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
@@ -40,11 +44,36 @@ public class HolderComponentTestModEntrypoint implements ModInitializer {
 		final Identifier second = Identifier.fromNamespaceAndPath("fabric", "second");
 		final Identifier third = Identifier.fromNamespaceAndPath("fabric", "third");
 
-		FabricDataComponentInitializers.registerInitializer(third, _ -> LOGGER.info("I should be third!"));
-		FabricDataComponentInitializers.registerInitializer(first, _ -> LOGGER.info("I should be first!"));
-		FabricDataComponentInitializers.registerInitializer(second, _ -> LOGGER.info("I should be second!"));
+		FabricDataComponentInitializers.registerInitializer(third, context -> {
+			LOGGER.info("I should be third!");
+			// Blue Shulker Color value will be overwritten if this initializer isn't third.
+			context.builder(BlockIds.DIRT).set(DataComponents.SHULKER_COLOR, DyeColor.BLUE);
+		});
+		FabricDataComponentInitializers.registerInitializer(first, context -> {
+			LOGGER.info("I should be first!");
+			// Red Sheep Color and Shulker Color should be overwritten if this initializer is first.
+			// Red Cat Color is set to verify that this initializer ran in the first place.
+			context.builder(BlockIds.DIRT)
+					.set(DataComponents.CAT_COLLAR, DyeColor.RED)
+					.set(DataComponents.SHEEP_COLOR, DyeColor.RED)
+					.set(DataComponents.SHULKER_COLOR, DyeColor.RED);
+		});
+		FabricDataComponentInitializers.registerInitializer(second, context -> {
+			LOGGER.info("I should be second!");
+			// Green Sheep Color will be overwritten if this initializer isn't second.
+			context.builder(BlockIds.DIRT)
+					.set(DataComponents.SHEEP_COLOR, DyeColor.GREEN)
+					.set(DataComponents.SHULKER_COLOR, DyeColor.GREEN);
+		});
 
 		FabricDataComponentInitializers.addInitializerOrdering(first, second);
 		FabricDataComponentInitializers.addInitializerOrdering(second, third);
+
+		final Identifier melonSeeds = Identifier.fromNamespaceAndPath("fabric", "melon_seeds");
+		FabricDataComponentInitializers.registerInitializer(melonSeeds, context ->
+				context.builder(ItemIds.MELON_SEEDS)
+						.set(DataComponents.PIG_VARIANT, context.lookupProvider().getOrThrow(PigVariants.TEMPERATE))
+						.set(DataComponents.ITEM_NAME, null)
+		);
 	}
 }
