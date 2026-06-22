9b008163c818ce33a3726a78d9a6296f96adfbc6
Fuzss <28218241+Fuzss@users.noreply.github.com>
2026-06-22 09:39:39 +0200

    add tests

--- fabric-screen-api-v1/src/client/java/net/fabricmc/fabric/mixin/screen/ScreenMixin.java
+++ fabric-screen-api-v1/src/client/java/net/fabricmc/fabric/mixin/screen/ScreenMixin.java
@@ -18,6 +18,8 @@ package net.fabricmc.fabric.mixin.screen;
 
 import java.util.List;
 
+import net.fabricmc.fabric.impl.client.screen.LegacyButtonList;
+
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -55,7 +57,7 @@ abstract class ScreenMixin implements ScreenExtensions {
 	private List<Renderable> renderables;
 
 	@Unique
-	private ButtonList fabricButtons;
+	private List<AbstractWidget> fabricButtons;
 	@Unique
 	private Event<ScreenEvents.Remove> removeEvent;
 	@Unique
