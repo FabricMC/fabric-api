1824ec2bda68801c4939a4259abb4e2960fafbcc
Shnupbups <shnupbups@gmail.com>
2026-07-01 23:51:35 +1000

    Fix typo in tag name

--- fabric-convention-tags-v2/src/main/java/net/fabricmc/fabric/api/tag/convention/v2/ConventionalBlockTags.java
+++ fabric-convention-tags-v2/src/main/java/net/fabricmc/fabric/api/tag/convention/v2/ConventionalBlockTags.java
@@ -259,7 +259,7 @@ public final class ConventionalBlockTags {
 	 * Tag that holds blocks that can be dyed despite already having a color, like wool.
 	 * (Does not include color blending blocks that would behave similar to leather armor item)
 	 */
-	public static final TagKey<Block> REDYEABLE_DYEABLE = register("dyeable/redyable");
+	public static final TagKey<Block> REDYEABLE_DYEABLE = register("dyeable/redyeable");
 
 	/**
 	 * Tag that holds blocks that can have dye applied to them, whether they have a color already or not.
