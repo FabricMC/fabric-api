7837acbacb9f6ccfc8ea5b32373e7c248cecf7f6
Cassian <cassian@godsted.com>
2026-07-09 13:19:30 -0400

    Extend `TooltipFlag` for recipe viewers

---
+++ fabric-item-api-v1/src/main/java/net/fabricmc/fabric/api/item/v1/FabricTooltipFlag.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2016, 2017, 2018, 2019 FabricMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.fabricmc.fabric.api.item.v1;
+
+import net.minecraft.world.item.TooltipFlag;
+
+/// General-purpose Fabric-provided extensions for [TooltipFlag].
+///
+/// Note: This interface is automatically implemented on all tooltip flags via interface injection.
+public interface FabricTooltipFlag {
+
+	/// {@return if the tooltip should provide recipe viewer specific information}
+	///
+	/// Modded tooltips often have requirements to hold keys like Shift or CTRL in order
+	/// to see more information. Recipe viewers, which commonly have to search tooltips,
+	/// should always be able to index this information regardless of whether a key is held.
+	default boolean isRecipeViewer() {
+		return false;
+	}
+}
