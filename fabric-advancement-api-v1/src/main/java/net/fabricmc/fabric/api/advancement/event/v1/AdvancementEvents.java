1ff691ae391384c3aced622b9f29a37631c1e477
Paulem79 <66211574+Paulem79@users.noreply.github.com>
2026-07-17 03:03:55 +0200

    feat: added structure of Modify event

--- fabric-advancement-api-v1/src/main/java/net/fabricmc/fabric/api/advancement/event/v1/AdvancementEvents.java
+++ fabric-advancement-api-v1/src/main/java/net/fabricmc/fabric/api/advancement/event/v1/AdvancementEvents.java
@@ -1,10 +1,31 @@
 package net.fabricmc.fabric.api.advancement.event.v1;
 
 
+import net.fabricmc.fabric.api.event.Event;
+import net.fabricmc.fabric.api.event.EventFactory;
+
 /**
  * Events for manipulating advancements.
  */
 public final class AdvancementEvents {
 	private AdvancementEvents() {
+	}
+
+	/**
+	 * This event can be used to modify advancements.
+	 * The main use case is to add items to vanilla or mod advancements (e.g. modded lava bucket to "Hot Stuff").
+	 */
+	public static final Event<Modify> MODIFY = EventFactory.createArrayBacked(Modify.class, listeners -> () -> {
+		for (Modify listener : listeners) {
+			listener.modifyAdvancement();
+		}
+	});
+
+	@FunctionalInterface
+	public interface Modify {
+		/**
+		 * Called when an advancement is loading to modify advancements.
+		 */
+		void modifyAdvancement();
 	}
 }
