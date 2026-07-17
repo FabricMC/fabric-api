657a7e74e656892a8d06598afe43ab58a7eb17f5
Paulem79 <66211574+paulem79@users.noreply.github.com>
2026-07-17 03:50:42 +0300

    feat: initial unfinished commit

---
+++ fabric-advancement-api-v1/src/main/java/net/fabricmc/fabric/api/advancement/event/v1/AdvancementEvents.java
@@ -0,0 +1,10 @@
+package net.fabricmc.fabric.api.advancement.event.v1;
+
+
+/**
+ * Events for manipulating advancements.
+ */
+public final class AdvancementEvents {
+	private AdvancementEvents() {
+	}
+}
