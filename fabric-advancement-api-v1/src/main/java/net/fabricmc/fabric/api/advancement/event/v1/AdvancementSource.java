241f2f503c692bd8c09af65fa3521d5b588eda50
Paulem79 <66211574+Paulem79@users.noreply.github.com>
2026-07-17 20:35:10 +0200

    perf: optimized AdvancementUtil source detection system, like loot table's one

--- fabric-advancement-api-v1/src/main/java/net/fabricmc/fabric/api/advancement/event/v1/AdvancementSource.java
+++ fabric-advancement-api-v1/src/main/java/net/fabricmc/fabric/api/advancement/event/v1/AdvancementSource.java
@@ -4,15 +4,26 @@ package net.fabricmc.fabric.api.advancement.event.v1;
  * Represents the origin of an advancement.
  */
 public enum AdvancementSource {
-	VANILLA,
-	MOD,
-	DATA_PACK,
-	REPLACED;
+	VANILLA(true),
+	MOD(true),
+	DATA_PACK(false),
+	REPLACED(false);
+
+	private final boolean builtin;
+
+	AdvancementSource(boolean builtin) {
+		this.builtin = builtin;
+	}
 
 	/**
-	 * @return true if the advancement comes from the vanilla game or a mod, false otherwise.
+	 * Returns whether this loot table source is builtin
+	 * and bundled in the vanilla or mod resources.
+	 *
+	 * <p>{@link #VANILLA} and {@link #MOD} are builtin.
+	 *
+	 * @return {@code true} if builtin, {@code false} otherwise
 	 */
 	public boolean isBuiltin() {
-		return this == VANILLA || this == MOD;
+		return builtin;
 	}
 }
--- fabric-advancement-api-v1/src/main/java/net/fabricmc/fabric/api/advancement/event/v1/AdvancementUtil.java
+++ fabric-advancement-api-v1/src/main/java/net/fabricmc/fabric/api/advancement/event/v1/AdvancementUtil.java
@@ -3,12 +3,15 @@ package net.fabricmc.fabric.api.advancement.event.v1;
 import net.fabricmc.fabric.impl.resource.pack.BuiltinModPackSource;
 import net.fabricmc.fabric.impl.resource.pack.ModResourcePackCreator;
 
+import net.minecraft.resources.Identifier;
 import net.minecraft.server.packs.repository.PackSource;
 import net.minecraft.server.packs.resources.Resource;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 public final class AdvancementUtil {
-	private AdvancementUtil() {
-	}
+	public static final Map<Identifier, AdvancementSource> SOURCES = new ConcurrentHashMap<>();
 
 	public static AdvancementSource determineSource(Resource resource) {
 		PackSource packSource = resource.getFabricPackSource();
@@ -20,6 +23,8 @@ public final class AdvancementUtil {
 		}
 
 		// If not builtin or mod, assume external data pack.
+		// It might also be a virtual advancement injected via mixin instead of being loaded
+		// from a resource, but we can't determine that here.
 		return AdvancementSource.DATA_PACK;
 	}
 }
