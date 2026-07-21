a51920eeaefc050760fabbef5d12b2d9b055cc17
cputnam-a11y <119390378+cputnam-a11y@users.noreply.github.com>
2026-07-20 23:40:00 -0500

    test holderset (it is not morning)

--- fabric-registry-sync-v0/src/test/java/net/fabricmc/fabric/test/registry/sync/BootstrapHelperTest.java
+++ fabric-registry-sync-v0/src/test/java/net/fabricmc/fabric/test/registry/sync/BootstrapHelperTest.java
@@ -27,12 +27,20 @@ import org.junit.jupiter.api.Test;
 import net.minecraft.SharedConstants;
 import net.minecraft.core.Holder;
 import net.minecraft.core.HolderLookup;
+import net.minecraft.core.HolderSet;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.data.worldgen.features.CaveFeatures;
 import net.minecraft.data.worldgen.features.MiscOverworldFeatures;
 import net.minecraft.data.worldgen.placement.MiscOverworldPlacements;
 import net.minecraft.resources.RegistryOps;
 import net.minecraft.server.Bootstrap;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.util.valueproviders.ConstantFloat;
+import net.minecraft.util.valueproviders.ConstantInt;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
+import net.minecraft.world.level.levelgen.feature.Feature;
+import net.minecraft.world.level.levelgen.feature.configurations.LargeDripstoneConfiguration;
 import net.minecraft.world.level.levelgen.placement.PlacedFeature;
 
 import net.fabricmc.fabric.api.event.registry.BootstrapHelper;
@@ -58,5 +66,29 @@ public class BootstrapHelperTest {
 		RegistryOps<JsonElement> ops = registries.createSerializationContext(JsonOps.INSTANCE);
 		Assertions.assertDoesNotThrow(() -> PlacedFeature.CODEC.encodeStart(ops, ops.getter(Registries.PLACED_FEATURE).orElseThrow().getOrThrow(MiscOverworldPlacements.BLUE_ICE)).getOrThrow());
 		Assertions.assertDoesNotThrow(() -> PlacedFeature.CODEC.encodeStart(ops, ops.getter(Registries.PLACED_FEATURE).orElseThrow().getOrThrow(MiscOverworldPlacements.DISK_GRAVEL)).getOrThrow());
+	}
+
+	@Test
+	void testFauxHolderSet() {
+		HolderSet.Named<Block> fakeTag = BootstrapHelper.createStandInHolderSet(BlockTags.ALL_SIGNS);
+		HolderLookup.Provider registries = BootstrapHelper.createEmptyBootstrappingLookup(pendingRegistries -> {
+			pendingRegistries.add(Registries.CONFIGURED_FEATURE, registry -> {
+				registry.register(CaveFeatures.LARGE_DRIPSTONE, new ConfiguredFeature<>(Feature.LARGE_DRIPSTONE, new LargeDripstoneConfiguration(
+						fakeTag,
+						47,
+						ConstantInt.of(14),
+						ConstantFloat.of(10f),
+						.9f,
+						ConstantFloat.of(5f),
+						ConstantFloat.of(5f),
+						ConstantFloat.of(1f),
+						50,
+						4f
+				)));
+			});
+		});
+
+		RegistryOps<JsonElement> ops = registries.createSerializationContext(JsonOps.INSTANCE);
+		Assertions.assertDoesNotThrow(() -> ConfiguredFeature.CODEC.encodeStart(ops, ops.getter(Registries.CONFIGURED_FEATURE).orElseThrow().getOrThrow(CaveFeatures.LARGE_DRIPSTONE)).getOrThrow());
 	}
 }
