fbfa1a82eae593c14823677f4c8f809b1d19ef87
IThundxr <me@ithundxr.dev>
2026-07-25 17:58:49 -0400

    fix missed error

--- fabric-renderer-indigo/src/client/java/net/fabricmc/fabric/impl/client/indigo/renderer/render/ExtendedItemFeatureRenderer.java
+++ fabric-renderer-indigo/src/client/java/net/fabricmc/fabric/impl/client/indigo/renderer/render/ExtendedItemFeatureRenderer.java
@@ -87,7 +87,7 @@ public class ExtendedItemFeatureRenderer extends RenderTypeFeatureRenderer<Exten
 		QuadEmitter emitter = this.emitter;
 		emitter.clear();
 
-		List<BakedQuad> vanillaQuads = submit.quads().all();
+		List<BakedQuad> vanillaQuads = submit.quads();
 
 		//noinspection ForLoopReplaceableByForEach
 		for (int i = 0; i < vanillaQuads.size(); i++) {
