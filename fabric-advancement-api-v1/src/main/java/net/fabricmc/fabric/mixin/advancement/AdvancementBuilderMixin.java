50206a2c8cbbdcf3bc46532119b90c81532a8926
Paulem79 <66211574+Paulem79@users.noreply.github.com>
2026-08-05 22:31:17 +0200

    chore(antstyle): applied spotless fixes

--- fabric-advancement-api-v1/src/main/java/net/fabricmc/fabric/mixin/advancement/AdvancementBuilderMixin.java
+++ fabric-advancement-api-v1/src/main/java/net/fabricmc/fabric/mixin/advancement/AdvancementBuilderMixin.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2016, 2017, 2018, 2019 FabricMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package net.fabricmc.fabric.mixin.advancement;
 
 import java.util.ArrayList;
@@ -17,14 +33,14 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 
-import net.fabricmc.fabric.api.advancement.v1.FabricAdvancementBuilder;
-
 import net.minecraft.advancements.Advancement;
 import net.minecraft.advancements.AdvancementRequirements;
 import net.minecraft.advancements.AdvancementRewards;
 import net.minecraft.advancements.DisplayInfo;
 import net.minecraft.advancements.triggers.Criterion;
 import net.minecraft.resources.Identifier;
+
+import net.fabricmc.fabric.api.advancement.v1.FabricAdvancementBuilder;
 
 @Mixin(Advancement.Builder.class)
 public abstract class AdvancementBuilderMixin implements FabricAdvancementBuilder {
