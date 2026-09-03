68a95250efea3c9f52112d82ef829e33b8da8a72
modmuss50 <modmuss50@gmail.com>
2026-09-03 09:15:58 +0100

    Reset FirstPersonHandsAndItemsRenderState

--- fabric-rendering-v1/src/client/java/net/fabricmc/fabric/mixin/client/rendering/renderstate/PlayerRenderStateMixin.java
+++ fabric-rendering-v1/src/client/java/net/fabricmc/fabric/mixin/client/rendering/renderstate/PlayerRenderStateMixin.java
@@ -16,19 +16,27 @@
 
 package net.fabricmc.fabric.mixin.client.rendering.renderstate;
 
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import net.minecraft.client.renderer.state.level.FirstPersonHandsAndItemsRenderState;
 import net.minecraft.client.renderer.state.level.PlayerRenderState;
 
 import net.fabricmc.fabric.api.client.rendering.v1.FabricRenderState;
 
 @Mixin(PlayerRenderState.class)
 abstract class PlayerRenderStateMixin {
+	@Shadow
+	@Final
+	public FirstPersonHandsAndItemsRenderState firstPersonHandsAndItems;
+
 	@Inject(method = "reset", at = @At("TAIL"))
 	private void clearExtraRenderData(CallbackInfo ci) {
 		((FabricRenderState) this).clearExtraData();
+		firstPersonHandsAndItems.clearExtraData();
 	}
 }
