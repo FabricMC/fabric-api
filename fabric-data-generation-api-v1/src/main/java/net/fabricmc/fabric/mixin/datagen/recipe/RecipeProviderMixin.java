cd926ed3cb899218abdec6ee08c8280bcf0b8a32
Patbox <39821509+Patbox@users.noreply.github.com>
2026-09-15 21:19:04 +0200

    Fix waxed recipe generation not being blocked off

--- fabric-data-generation-api-v1/src/main/java/net/fabricmc/fabric/mixin/datagen/recipe/RecipeProviderMixin.java
+++ fabric-data-generation-api-v1/src/main/java/net/fabricmc/fabric/mixin/datagen/recipe/RecipeProviderMixin.java
@@ -19,12 +19,17 @@ package net.fabricmc.fabric.mixin.datagen.recipe;
 import com.llamalad7.mixinextras.sugar.Local;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.data.recipes.RecipeProvider;
 import net.minecraft.data.recipes.packs.VanillaRecipeProvider;
+import net.minecraft.resources.Identifier;
+import net.minecraft.world.flag.FeatureFlagSet;
 import net.minecraft.world.level.ItemLike;
+import net.minecraft.world.level.block.Block;
 
 @Mixin(RecipeProvider.class)
 abstract class RecipeProviderMixin {
@@ -32,11 +37,33 @@ abstract class RecipeProviderMixin {
 	// in the `minecraft` namespace. Override this method to place
 	// them in the output item’s namespace instead.
 	@ModifyArg(method = "stonecutterResultFromBase(Lnet/minecraft/data/recipes/RecipeCategory;Lnet/minecraft/world/level/ItemLike;Lnet/minecraft/world/level/ItemLike;I)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/data/recipes/SingleItemRecipeBuilder;save(Lnet/minecraft/data/recipes/RecipeOutput;Ljava/lang/String;)V"), index = 1)
-	private String adjustId(String path, @Local(name = "result", argsOnly = true) ItemLike result) {
+	private String adjustIdStonecutter(String path, @Local(name = "result", argsOnly = true) ItemLike result) {
 		if ((Object) this instanceof VanillaRecipeProvider) {
 			return path;
 		}
 
 		return BuiltInRegistries.ITEM.getKey(result.asItem()).getNamespace() + ":" + path;
+	}
+
+	// Same as above
+	@ModifyArg(method = "lambda$waxRecipes$0", at = @At(value = "INVOKE", target = "Lnet/minecraft/data/recipes/ShapelessRecipeBuilder;save(Lnet/minecraft/data/recipes/RecipeOutput;Ljava/lang/String;)V"), index = 1)
+	private String adjustIdWaxRecipes(String path, @Local(name = "waxedBlock", argsOnly = true) Block waxedBlock) {
+		if ((Object) this instanceof VanillaRecipeProvider) {
+			return path;
+		}
+
+		return BuiltInRegistries.BLOCK.getKey(waxedBlock).getNamespace() + ":" + path;
+	}
+
+	@Inject(method = "lambda$waxRecipes$0", at = @At("HEAD"), cancellable = true)
+	private void dontGenerateNonVanillaWaxingRecipes(FeatureFlagSet flagSet, Block block, Block waxedBlock, CallbackInfo ci) {
+		if ((Object) this instanceof VanillaRecipeProvider) {
+			Identifier a = BuiltInRegistries.BLOCK.getKey(block);
+			Identifier b = BuiltInRegistries.BLOCK.getKey(waxedBlock);
+
+			if (a.getNamespace().equals(b.getNamespace()) && !a.getNamespace().equals(Identifier.DEFAULT_NAMESPACE)) {
+				ci.cancel();
+			}
+		}
 	}
 }
